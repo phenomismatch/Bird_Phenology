d5947414ee35497995097bd1b5c83b98ffcb8b7b
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-10-14 10:38:27 -0400

    Change to 8GB RAM

--- Scripts/eBird_Nestwatch/2-logit-cubic/species/2-Setophaga_discolor.sh
+++ Scripts/eBird_Nestwatch/2-logit-cubic/species/2-Setophaga_discolor.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 4 #cpus
 #SBATCH --qos=general #queue (same as partition)
 #SBATCH --partition=general #partition - can also specify 'himem'
-#SBATCH --mem=10G #memory requested
+#SBATCH --mem=8G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
 #SBATCH -o lc-Setophaga_discolor.out #STDOUT
