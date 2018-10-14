5be6334cbfc5f5d1764b03afc5a8dc4743f4d6e2
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-10-14 12:44:07 -0400

    new db query - create dirs, further processing

--- Scripts/eBird_Nestwatch/1b-query-ebird.R
+++ Scripts/eBird_Nestwatch/1b-query-ebird.R
@@ -18,7 +18,7 @@ cy_dir <- '~/Google_Drive/R/'
 library(dplyr)
 library(RPostgreSQL)
 library(DBI)
-
+library(dggridR)
 
 # set wd ------------------------------------------------------------------
 
@@ -111,47 +111,16 @@ cxn <- DBI::dbConnect(pg,
 
 # Query and filter ----------------------------------------------------
 
-data <- DBI::dbGetQuery(cxn, paste0("
-                                    SELECT event_id, year, day, place_id, lat, lng, started, 
-                                    count, radius, sci_name, common_name,
-                                    (event_json ->> 'ALL_SPECIES_REPORTED')::int AS all_species_reported,
-                                    (event_json ->> 'DURATION_MINUTES')::int AS duration_minutes,
-                                    count_json ->> 'OBSERVER_ID' AS observer_id,
-                                    count_json ->> 'BREEDING_BIRD_ATLAS_CODE' AS BBA_code,
-                                    (event_json ->> 'NUMBER_OBSERVERS')::int AS number_observers,
-                                    event_json ->> 'GROUP_IDENTIFIER' AS group_identifier
-                                    FROM places
-                                    JOIN events USING (place_id)
-                                    JOIN counts USING (event_id)
-                                    JOIN taxons USING (taxon_id)
-                                    WHERE dataset_id = 'ebird'
-                                    AND year > 2001
-                                    AND day < 200
-                                    AND lng BETWEEN -100 AND -50
-                                    AND lat > 26
-                                    AND (sci_name IN (", SL,"))
-                                    AND (event_json ->> 'ALL_SPECIES_REPORTED')::int = 1
-                                    AND (event_json ->> 'DURATION_MINUTES')::int BETWEEN 6 AND 1440
-                                    AND LEFT(started, 2)::int < 16
-                                    AND RADIUS < 100000
-                                    LIMIT 500;
-                                    "))
-
-str(data)
-head(data)
-
-
 #*get info for each unique event
 #*create 153 species columns with NA
-#*in a loop, query each species individually and fill NAs
-#*change all remaining NA values to 0
+#*in a loop, query each species individually and fill obs with 1s, no obs with 0s
 #*merge with hex cells
 #*create rds objects for each species
 
 
-#all unique event_ids that meet criteria
+#filter all unique event_ids that meet criteria - about 42 min to complete query
 tt <- proc.time()
-data2 <- DBI::dbGetQuery(cxn, paste0("
+data <- DBI::dbGetQuery(cxn, paste0("
                                      SELECT DISTINCT ON (event_id) event_id, year, day, place_id, lat, lng, started, 
                                      radius,
                                      (event_json ->> 'ALL_SPECIES_REPORTED')::int AS all_species_reported,
@@ -175,15 +144,111 @@ data2 <- DBI::dbGetQuery(cxn, paste0("
                                      AND LEFT(started, 2)::int < 16
                                      AND RADIUS < 100000;
                                      "))
-
-
 proc.time() - tt
 
-str(data2)
-head(data2)
+#only values with unique group identifiers (or no group identifier)
+data2 <- data[!duplicated(data[,'group_identifier'], 
+                          incomparables = NA),]
 
-#select unique group IDs when not NULL
-unique((data2$event_id))
+#create species columns
+data2[species_list_i[,1]] <- NA
+
 #zero fill
+nsp <- NROW(species_list_i)
 
+#~2-3 minutes for each species query
+for (i in 1:nsp)
+{
+  #i <- 1
+  tt <- proc.time()
+  temp <- DBI::dbGetQuery(cxn, paste0("
+                                     SELECT event_id, year, day, place_id, lat, lng, started, radius, 
+                                       sci_name, common_name, count,
+                                       (event_json ->> 'ALL_SPECIES_REPORTED')::int AS all_species_reported,
+                                       (event_json ->> 'DURATION_MINUTES')::int AS duration_minutes,
+                                       count_json ->> 'OBSERVER_ID' AS observer_id,
+                                       count_json ->> 'BREEDING_BIRD_ATLAS_CODE' AS BBA_code,
+                                       (event_json ->> 'NUMBER_OBSERVERS')::int AS number_observers,
+                                       event_json ->> 'GROUP_IDENTIFIER' AS group_identifier
+                                       FROM places
+                                       JOIN events USING (place_id)
+                                       JOIN counts USING (event_id)
+                                       JOIN taxons USING (taxon_id)
+                                       WHERE dataset_id = 'ebird'
+                                       AND year > 2001
+                                       AND day < 200
+                                       AND lng BETWEEN -100 AND -50
+                                       AND lat > 26
+                                       AND (sci_name IN ('", species_list_i2[i],"'))
+                                       AND (event_json ->> 'ALL_SPECIES_REPORTED')::int = 1
+                                       AND (event_json ->> 'DURATION_MINUTES')::int BETWEEN 6 AND 1440
+                                       AND LEFT(started, 2)::int < 16
+                                       AND RADIUS < 100000;
+                                       "))
+  proc.time() - tt
+  
+  #indices to fill with 1s (observations of species made for these events)
+  ind <- which(data2$event_id %in% temp$event_id)
+  
+  #indices to fill with 0s (observations of species not made for these events)
+  n_ind <- (1:NROW(data2))[-ind]
+  
+  #fill observersations for species i with 1s
+  data2[ind, paste0(species_list_i[i,1])] <- 1
+  
+  #fill no observations for species i with 0s
+  data2[n_ind, paste0(species_list_i[i,1])] <- 0
+}
+
+
+
+# add jday^2, jday^3 and shr ---------------------------------------------------
+
+#scaled julian day, scaled julian day^2, and scaled julian day^3
+SJDAY  <- as.vector(scale(data3$day, scale = FALSE))
+SJDAY2 <- as.vector(scale(data3$day^2, scale = FALSE))
+SJDAY3 <- as.vector(scale(data3$day^3, scale = FALSE))
+
+data2$sjday <- SJDAY
+data2$sjday2 <- SJDAY2
+data2$sjday3 <- SJDAY3
+
+#scaled effort hours
+SHR <- as.vector(scale((data3$duration_minutes/60)))
+
+data2$shr <- SHR
+
+
+
+# bin to hex grid ---------------------------------------------------------
+
+# Construct geospatial hexagonal grid
+
+hexgrid6 <- dggridR::dgconstruct(res = 6) 
+data2$cell6 <- dggridR::dgGEO_to_SEQNUM(hexgrid6, 
+                                        in_lon_deg = data2$lng, 
+                                        in_lat_deg = data2$lat)[[1]]
+
+
+dir.create(paste0('Processed/db_query_', Sys.Date()))
+setwd(paste0('Processed/db_query_', Sys.Date()))
+
+#save to rds object
+saveRDS(data2, file = 'ebird_NA_phen_proc_ALL_SPECIES.rds')
+
+
+
+
+# Different rds object each species ---------------------------------------
+
+for(i in 1:nsp)
+{
+  #i <- 1
+  sdata <- dplyr::select(data2, 
+                         year, day, sjday, sjday2, 
+                         sjday3, shr, cell6, species_list_i[i,1])
+  names(sdata)[8] <- "detect"
+  
+  saveRDS(sdata, file = paste0('ebird_NA_phen_proc_',species_list[i], '.rds'))
+}
 
