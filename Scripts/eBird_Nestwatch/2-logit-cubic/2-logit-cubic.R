64591a26e9aa668d4483c4aa0015f0f45a8fc7e7
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-10-16 12:29:23 -0400

    prep for xanadu test

--- Scripts/eBird_Nestwatch/2-logit-cubic/2-logit-cubic.R
+++ Scripts/eBird_Nestwatch/2-logit-cubic/2-logit-cubic.R
@@ -88,7 +88,7 @@ fit_diag <- halfmax_matrix_list <- list()
 # fit logit cubic ---------------------------------------------------------
 
 #number of iterations each model should be run
-ITER <- 10
+ITER <- 2
 
 
 #loop through each species, year, cell and extract half-max parameter
