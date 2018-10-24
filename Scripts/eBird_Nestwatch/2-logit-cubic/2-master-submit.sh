e9ed59119299e6df8d5e0f83248703b5a064d828
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-10-24 10:04:30 -0400

    copy script into halfmax dir

--- Scripts/eBird_Nestwatch/2-logit-cubic/2-master-submit.sh
+++ Scripts/eBird_Nestwatch/2-logit-cubic/2-master-submit.sh
@@ -7,3 +7,6 @@ do
   sbatch species/2-$temp.sh
 done < ../../../Data/eBird_species_list.txt
 #done < ../../../Data/test_species_list.txt
+
+DATE=`date +%Y-%m-%d`
+cp 2-logit-cubic.R ../../../Data/Processed/halfmax_species/2-logit-cubic-$DATE.R
