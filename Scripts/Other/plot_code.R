0d449e79a8e3b2f9a369beab2b4f0fdf050c8318
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-11-02 14:13:10 -0400

    clean up

--- Scripts/Other/plot_code.R
+++ Scripts/Other/plot_code.R
@@ -1,6 +1,52 @@
 
+# Top-level dir -----------------------------------------------------------
 
-# Plot pre-IAR halfmax estimates ------------------------------------------
+#desktop/laptop
+dir <- '~/Google_Drive/R/'
+
+#Xanadu
+#dir <- '/home/CAM/cyoungflesh/phenomismatch/'
+
+
+
+
+# load packages -----------------------------------------------------------
+
+library(ggplot2)
+
+
+
+# db/hm query dir ------------------------------------------------------------
+
+db_dir <- 'db_query_2018-10-15'
+hm_dir <- 'halfmax_species_2018-10-16'
+IAR_dir <- 'IAR_2018-10-26'
+
+
+
+setwd(paste0(dir, 'Bird_Phenology/Data/Processed/', IAR_dir))
+
+IAR_date <- substr(IAR_dir, start = 5, stop = 15)
+
+diagnostics_frame_p <- readRDS(paste0('diagnostics_frame-', IAR_date,'.rds'))
+cells_frame <- readRDS(paste0('cells_frame-', IAR_date, '.rds'))
+yrs_frame <- readRDS(paste0('yrs_frame-', IAR_date, '.rds'))
+
+
+#which species/year has the most cells - to model 'data rich species'
+DR_sp <- as.character(yrs_frame[which.max(yrs_frame[,1:3]$n_cells),1])
+DR_filt <- dplyr::filter(yrs_frame, species == DR_sp)[,1:3]
+DR_yr <- c(2002, 2010, 2017)
+
+
+
+# plot cell numbers on map ------------------------------------------------
+
+#create hex cells
+hexgrid6 <- dggridR::dgconstruct(res = 6)
+cells <- unique(diagnostics_frame$cell)
+ncel <- length(cells)
+
 
 
 #transform cells to grid
@@ -20,11 +66,11 @@ mexicomap <- data.frame(maps::map("world", "Mexico", plot = FALSE)[c("x", "y")])
 #plot
 p <- ggplot() +
   geom_path(data = usamap, 
-            aes(x = x, y = y), color = 'black') + 
+            aes(x = x, y = y), color = 'black', alpha = 0.5) + 
   geom_path(data = canadamap, 
-            aes(x = x, y = y), color = 'black') + 
+            aes(x = x, y = y), color = 'black', alpha = 0.5) + 
   geom_path(data = mexicomap, 
-            aes(x = x, y = y), color = 'black') + 
+            aes(x = x, y = y), color = 'black', alpha = 0.5) + 
   coord_map("ortho", orientation = c(35, -80, 0), 
             xlim = c(-100, -55), ylim = c(20, 90)) + 
   geom_polygon(data = to_plt2, aes(x = long, y = lat, group = group), fill = 'red', alpha = 0.3) +
@@ -40,18 +86,18 @@ p <- ggplot() +
 p
 
 
-#cells that should probbaly be removed (can be filtered more formally at the query stage in the future)
-cells_to_rm <- c(208, 845, 289, 316, 343, 345, 346, 319, 320, 373, 
-                 401, 402, 375, 376, 402, 403, 404, 370, 770, 796, 
-                 766, 765, 763, 735, 734, 731, 758, 813, 3669, 397,
-                 398)
 
 
 
+# filter cells ------------------------------------------------------------
+
+#cells identified manually from plot (plot_code.R) to filter
+cells_to_keep <- c(622, 595, 567, 594, 621, 648, 649, 676, 675, 647, 619, 
+                   620, 621, 593, 592, 619, 618, 591, 564, 565, 566, 646, 
+                   674, 702, 703)
 
 
-'%ni%' <- Negate('%in%')
-to_plt3 <- to_plt2[which(to_plt2$cell %ni% cells_to_rm),]
+to_plt3 <- to_plt2[which(to_plt2$cell %in% cells_to_keep),]
 
 
 #plot
