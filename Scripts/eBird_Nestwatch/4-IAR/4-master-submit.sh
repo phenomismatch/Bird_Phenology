3af40d42fadf1eb1e76f508187f1be933c0d4bf2
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-11-12 01:20:19 -0500

    master submit IAR

--- Scripts/eBird_Nestwatch/4-IAR/4-master-submit.sh
+++ Scripts/eBird_Nestwatch/4-IAR/4-master-submit.sh
@@ -4,9 +4,9 @@ while read name
 do
   temp="${name%\"}"
   temp="${temp#\"}"
-  sbatch species/2-$temp.sh
-done < ../../../Data/eBird_species_list.txt
+  sbatch species/4-$temp.sh
+done < ../../../Data/IAR_species_list.txt
 #done < ../../../Data/test_species_list.txt
 
 DATE=`date +%Y-%m-%d`
-cp 2-logit-cubic.R ../../../Data/Processed/halfmax_species/2-logit-cubic-$DATE.R
+cp 4-IAR-model.R ../../../Data/Processed/IAR_2018-11-12/4-IAR-model-$DATE.R
