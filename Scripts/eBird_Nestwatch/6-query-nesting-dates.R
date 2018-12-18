1e10ad6bce36aade05e145b1c65451784bb2147e
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-12-18 11:04:45 -0500

    add query nesting dates

---
+++ Scripts/eBird_Nestwatch/6-query-nesting-dates.R
@@ -0,0 +1,151 @@
+
+
+
+
+# top-level dir --------------------------------------------------------------
+
+dir <- '~/Google_Drive/R/'
+
+
+# Load packages -----------------------------------------------------------
+
+library(dplyr)
+library(RPostgreSQL)
+library(DBI)
+library(dggridR)
+library(doParallel)
+library(foreach)
+
+# set wd ------------------------------------------------------------------
+
+setwd(paste0(dir, 'Bird_Phenology/Data/'))
+
+
+
+# import eBird species list -----------------------------------------------------
+
+species_list_i <- read.table('IAR_species_list.txt', stringsAsFactors = FALSE)
+
+#remove underscore and coerce to vector
+species_list_i2 <- as.vector(apply(species_list_i, 2, function(x) gsub("_", " ", x)))
+
+#combine species names into a single string with quotes
+SL <- paste0("'", species_list_i2, "'", collapse = ", ")
+
+
+# access DB ---------------------------------------------------------------
+
+pass <- readLines('db_pass.txt')
+
+pg <- DBI::dbDriver("PostgreSQL")
+
+cxn <- DBI::dbConnect(pg, 
+                      user = "cyoungflesh", 
+                      password = pass, 
+                      host = "35.221.16.125", 
+                      port = 5432, 
+                      dbname = "sightings")
+
+
+
+# create query dir and navigate there -------------------------------------------
+
+
+# query_dir_path <- paste0('Processed/db_query_', Sys.Date())
+# 
+# dir.create(query_dir_path)
+# setwd(query_dir_path)
+# 
+
+
+
+# filter dataset notes ----------------------------------------------------------
+
+#SQL FILTER BY:
+# * only ebird data
+# * species of interest (import from text file)
+# * only complete checklists
+# * duration minutes (6 min to 24 hours)
+# * lat (> 26) and lon (-100 to -50)
+# * year > 2001
+# * day of year (< julian day 200)
+# * time started before 16:00
+# * radius < 100km
+
+
+#FILTERED IN R:
+# * only unique groups when not null
+
+
+#EBIRD DB FIELDS FROM FROM RAFE
+# place_json contains:
+# COUNTRY_CODE
+# STATE_CODE
+# COUNTY_CODE
+# IBA_CODE
+# BCR_CODE
+# USFWS_CODE
+# ATLAS_BLOCK
+# LOCALITY_ID
+# LOCALITY_TYPE
+# EFFORT_AREA_HA
+# 
+# event_json contains:
+# SAMPLING_EVENT_IDENTIFIER
+# EFFORT_AREA_HA
+# APPROVED
+# REVIEWED
+# NUMBER_OBSERVERS
+# ALL_SPECIES_REPORTED
+# OBSERVATION_DATE
+# GROUP_IDENTIFIER
+# DURATION_MINUTES
+# 
+# counts_json contains:
+# SCIENTIFIC_NAME
+# GLOBAL_UNIQUE_IDENTIFIER
+# LAST_EDITED_DATE
+# TAXONOMIC_ORDER
+# CATEGORY
+# SUBSPECIES_SCIENTIFIC_NAME
+# BREEDING_BIRD_ATLAS_CODE
+# BREEDING_BIRD_ATLAS_CATEGORY
+# AGE_SEX
+# OBSERVER_ID
+# HAS_MEDIA
+
+
+
+
+# Query and filter - event_id ----------------------------------------------------
+
+#filter all unique event_ids that meet criteria - about 38 min to complete query
+
+data <- DBI::dbGetQuery(cxn, paste0("
+                                    SELECT DISTINCT ON (event_id) event_id, year, day, place_id, lat, lng, started, 
+                                    radius,
+                                    (event_json ->> 'ALL_SPECIES_REPORTED')::int AS all_species_reported,
+                                    (event_json ->> 'DURATION_MINUTES')::int AS duration_minutes,
+                                    count_json ->> 'OBSERVER_ID' AS observer_id,
+                                    count_json ->> 'BREEDING_BIRD_ATLAS_CODE' AS BBA_code,
+                                    (event_json ->> 'NUMBER_OBSERVERS')::int AS number_observers,
+                                    event_json ->> 'GROUP_IDENTIFIER' AS group_identifier
+                                    count_json ->> 'BREEDING_BIRD_ATLAS_CODE' AS BBA_code
+                                    count_json ->> 'BREEDING_BIRD_ATLAS_CATEGORY' AS BBA_category
+                                    FROM places
+                                    JOIN events USING (place_id)
+                                    JOIN counts USING (event_id)
+                                    JOIN taxons USING (taxon_id)
+                                    WHERE dataset_id = 'ebird'
+                                    AND year > 2001
+                                    AND day < 200
+                                    AND lng BETWEEN -100 AND -50
+                                    AND lat > 26
+                                    AND (sci_name IN (", SL,"))
+                                    AND (event_json ->> 'ALL_SPECIES_REPORTED')::int = 1
+                                    AND (event_json ->> 'DURATION_MINUTES')::int BETWEEN 6 AND 1440
+                                    AND LEFT(started, 2)::int < 16
+                                    AND RADIUS < 100000;
+                                    "))
+
+
