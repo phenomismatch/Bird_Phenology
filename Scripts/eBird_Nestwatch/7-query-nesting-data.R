522526f05e97a4afd40a67a9180a523fc1abc0ab
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-12-19 10:45:33 -0500

    add to ebird breeding code query

--- Scripts/eBird_Nestwatch/7-query-nesting-data.R
+++ Scripts/eBird_Nestwatch/7-query-nesting-data.R
@@ -7,6 +7,47 @@
 ####################
 
 
+# top-level dir --------------------------------------------------------------
+
+dir <- '~/Google_Drive/R/'
+
+
+# Load packages -----------------------------------------------------------
+
+library(dplyr)
+library(RPostgreSQL)
+library(DBI)
+library(dggridR)
+library(doParallel)
+library(foreach)
+
+
+# import IAR data ---------------------------------------------------------
+
+setwd(paste0(dir, 'Bird_Phenology/Data/Processed/'))
+
+IAR_out_dir <- 'IAR_output_2018-11-12'
+IAR_out_date <- substr(IAR_out_dir, start = 12, stop = 21)
+
+IAR_data <- readRDS(paste0('master_arrival_', IAR_out_date, '.rds'))
+
+
+
+# import IAR species list -----------------------------------------------------
+
+setwd(paste0(dir, 'Bird_Phenology/Data/'))
+
+species_list_i <- read.table('IAR_species_list.txt', stringsAsFactors = FALSE)
+
+#remove underscore and coerce to vector
+species_list_i2 <- as.vector(apply(species_list_i, 2, function(x) gsub("_", " ", x)))
+
+
+
+# create hex grid ---------------------------------------------------------
+
+hexgrid6 <- dggridR::dgconstruct(res = 6) 
+
 
 
 # eBird breeding codes ----------------------------------------------------
@@ -130,18 +171,163 @@ data <- DBI::dbGetQuery(cxn, paste0("
 
 
 
+#only values with unique group identifiers (or no group identifier)
+data2 <- data[!duplicated(data[,'group_identifier'], 
+                          incomparables = NA),]
+
+rm(data)
+
+
+
+
+# bin lat/lon to hex grid and add to data ---------------------------------------------------------
+
+# Construct geospatial hexagonal grid
+
+hexgrid6 <- dggridR::dgconstruct(res = 6) 
+data2$cell6 <- dggridR::dgGEO_to_SEQNUM(hexgrid6, 
+                                        in_lon_deg = data2$lng, 
+                                        in_lat_deg = data2$lat)[[1]]
+
+
+
+
+# query individual species, zero fill, and create RDS objects ----------------------------------
+
+
+#create species columns
+data2[species_list_i[,1]] <- NA
+
+nsp <- NROW(species_list_i)
+
+#run in parallel with 7 logical cores
+doParallel::registerDoParallel(cores = 7)
+
+tt <- proc.time()
+foreach::foreach(i = 1:nsp) %dopar%
+{
+  #i <- 89
+  print(i)
+  
+  pg <- DBI::dbDriver("PostgreSQL")
+  
+  cxn <- DBI::dbConnect(pg, 
+                        user = "cyoungflesh", 
+                        password = pass, 
+                        host = "35.221.16.125", 
+                        port = 5432, 
+                        dbname = "sightings")
+  
+  temp <- DBI::dbGetQuery(cxn, paste0("SELECT event_id
+                                      FROM events
+                                      JOIN places USING (place_id)
+                                      JOIN counts USING (event_id)
+                                      JOIN taxons USING (taxon_id)
+                                      WHERE dataset_id = 'ebird'
+                                      AND year > 2001
+                                      AND day < 200
+                                      AND lng BETWEEN -100 AND -50
+                                      AND lat > 26
+                                      AND (sci_name IN ('", species_list_i2[i],"'));
+                                      "))
+  
+  #indices to fill with 1s (observations of species made for these events)
+  ind <- which(data2$event_id %in% temp$event_id)
+  
+  #indices to fill with 0s (observations of species not made for these events)
+  n_ind <- (1:NROW(data2))[-ind]
+  
+  #fill observersations for species i with 1s
+  data2[ind, species_list_i[i,1]] <- 1
+  
+  #fill no observations for species i with 0s
+  data2[n_ind, species_list_i[i,1]] <- 0
+  
+  sdata <- dplyr::select(data2, 
+                         year, day, sjday, sjday2, 
+                         sjday3, shr, cell6, species_list_i[i,1])
+  
+  names(sdata)[8] <- "detect"
+  sdata['species'] <- species_list_i[i,1]
+  
+  saveRDS(sdata, file = paste0('ebird_NA_phen_proc_', species_list_i[i,1], '.rds'))
+}
+proc.time() - tt
+
+
+
+# Find files that weren’t created -----------------------------------------
+
+fls <- list.files()
+nms <- c()
+for (i in 1:length(fls))
+{
+  #i <- 1
+  nms <- c(nms, substr(fls[i], 20, (nchar(fls[i]) - 4)))
+}
+
+#missed species
+m_sp <- species_list_i[which(!species_list_i[,1] %in% nms),1]
+#remove underscore
+m_sp2 <- gsub("_", " ", m_sp)
+
+#create those files
+for (i in 1:length(m_sp2))
+{
+  #i <- 1
+  print(i)
+  
+  pg <- DBI::dbDriver("PostgreSQL")
+  
+  cxn <- DBI::dbConnect(pg, 
+                        user = "cyoungflesh", 
+                        password = pass, 
+                        host = "35.221.16.125", 
+                        port = 5432, 
+                        dbname = "sightings")
+  
+  temp <- DBI::dbGetQuery(cxn, paste0("SELECT event_id
+                                      FROM events
+                                      JOIN places USING (place_id)
+                                      JOIN counts USING (event_id)
+                                      JOIN taxons USING (taxon_id)
+                                      WHERE dataset_id = 'ebird'
+                                      AND year > 2001
+                                      AND day < 200
+                                      AND lng BETWEEN -100 AND -50
+                                      AND lat > 26
+                                      AND (sci_name IN ('", m_sp2[i],"'));
+                                      "))
+  
+  #indices to fill with 1s (observations of species made for these events)
+  ind <- which(data2$event_id %in% temp$event_id)
+  
+  #indices to fill with 0s (observations of species not made for these events)
+  n_ind <- (1:NROW(data2))[-ind]
+  
+  #fill observersations for species i with 1s
+  data2[ind, m_sp2[i]] <- 1
+  
+  #fill no observations for species i with 0s
+  data2[n_ind, m_sp2[i]] <- 0
+  
+  sdata <- dplyr::select(data2, 
+                         year, day, sjday, sjday2, 
+                         sjday3, shr, cell6, m_sp2[i])
+  
+  names(sdata)[8] <- "detect"
+  sdata['species'] <- m_sp2[i]
+  
+  saveRDS(sdata, file = paste0('ebird_NA_phen_proc_', m_sp[i], '.rds'))
+}
 
 
 
 
 
-# MAPS observations -------------------------------------------------------
 
 
 
 
 
-
-
-# MAPS captures -----------------------------------------------------------
 
