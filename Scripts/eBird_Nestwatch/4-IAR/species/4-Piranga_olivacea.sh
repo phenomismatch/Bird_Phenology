3e05ca26166d258bd436486b3b4f83cb3a76fc83
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-16 21:14:42 -0500

    prep for xanadu test

--- Scripts/eBird_Nestwatch/4-IAR/species/4-Piranga_olivacea.sh
+++ Scripts/eBird_Nestwatch/4-IAR/species/4-Piranga_olivacea.sh
@@ -9,20 +9,17 @@
 #SBATCH --mem=8G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
-#SBATCH -o /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2018-11-30/iar-Piranga_olivacea.out #STDOUT
-#SBATCH -e /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2018-11-30/iar-Piranga_olivacea.err #STDERR
+#SBATCH -o /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2018-01-16/iar-Piranga_olivacea.out #STDOUT
+#SBATCH -e /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2018-01-16/iar-Piranga_olivacea.err #STDERR
 
 #echos name of node
 echo `hostname`
 
-#load R module
-module load R/3.5.1
+#load singularity module
+module load singularity/3.0.2
 
-#R library
-#export R_LIBS=/home/CAM/cyoungflesh/R_libs
-
-#run R script - species arg
-Rscript 4-IAR-model.R Piranga_olivacea
+#run R script using singularity - species arg
+singularity exec /home/CAM/cyoungflesh/R.sif Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Piranga_olivacea
 
 #displays amount of memory used
 sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
