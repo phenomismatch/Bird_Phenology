d9392a9baf8770eb8814d61ac4c9e76b84ccf9a1
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-17 15:09:59 -0500

    new run scripts

--- Scripts/eBird_Nestwatch/2-logit-cubic/species/2-Setophaga_tigrina.sh
+++ Scripts/eBird_Nestwatch/2-logit-cubic/species/2-Setophaga_tigrina.sh
@@ -15,11 +15,13 @@
 #echos name of node
 echo `hostname`
 
-#load R module (through singularity)
-module load R/3.5.2
+#load R module (auto loads singularity and run script)
+#module load R/3.5.2
+#Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/2-logit-cubic/2-logit-cubic.R Setophaga_tigrina
 
-#run R script using singularity - species arg
-Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/2-logit-cubic/2-logit-cubic.R Setophaga_tigrina
+#load singularity and run R script using singularity
+module load singularity 3.0.2
+singularity exec /home/CAM/cyoungflesh/R.sif Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/2-logit-cubic/2-logit-cubic.R Setophaga_tigrina
 
 #displays amount of memory used
 sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
