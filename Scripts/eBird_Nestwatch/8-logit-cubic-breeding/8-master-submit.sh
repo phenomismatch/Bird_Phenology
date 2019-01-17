c47d0eb7b34048ed5eaea5552bc2ff1f027a41ec
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-17 15:42:00 -0500

    full run Xanadu 8

--- Scripts/eBird_Nestwatch/8-logit-cubic-breeding/8-master-submit.sh
+++ Scripts/eBird_Nestwatch/8-logit-cubic-breeding/8-master-submit.sh
@@ -5,8 +5,8 @@ do
   temp="${name%\"}"
   temp="${temp#\"}"
   sbatch species/8-$temp.sh
-#done < ../../../Data/IAR_species_list.txt
-done < ../../../Data/test_species_list.txt
+done < ../../../Data/IAR_species_list.txt
+#done < ../../../Data/test_species_list.txt
 
 DATE="2019-01-16"
 cp 8-logit-cubic-breeding.R ../../../Data/Processed/halfmax_breeding_$DATE/8-logit-cubic-breeding-$DATE.R
