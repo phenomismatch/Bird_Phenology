33be68e8b8b15f4ddd8704882af84bc19341bed0
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-18 10:24:54 -0500

    new run scripts 4

--- Scripts/eBird_Nestwatch/4-IAR/species/4-Parkesia_motacilla.sh
+++ Scripts/eBird_Nestwatch/4-IAR/species/4-Parkesia_motacilla.sh
@@ -9,20 +9,20 @@
 #SBATCH --mem=8G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
-#SBATCH -o /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Parkesia_motacilla.out #STDOUT
-#SBATCH -e /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Parkesia_motacilla.err #STDERR
+#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Parkesia_motacilla.out #STDOUT
+#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Parkesia_motacilla.err #STDERR
 
 #echos name of node
 echo `hostname`
 
 #load R module (auto loads singularity and run script)
 #module load R/3.5.2
-#Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Parkesia_motacilla
+#Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Parkesia_motacilla
 
 #load singularity and run R script using singularity
 module load singularity/3.0.2
-#singularity exec /home/CAM/cyoungflesh/R.sif Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Parkesia_motacilla
-singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Parkesia_motacilla
+#singularity exec /home/CAM/cyoungflesh/R.sif Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Parkesia_motacilla
+singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Parkesia_motacilla
 
 
 #displays amount of memory used
--- Scripts/eBird_Nestwatch/4-IAR/species/4-Setophaga_pensylvanica.sh
+++ Scripts/eBird_Nestwatch/4-IAR/species/4-Setophaga_pensylvanica.sh
@@ -9,20 +9,20 @@
 #SBATCH --mem=8G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
-#SBATCH -o /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Setophaga_pensylvanica.out #STDOUT
-#SBATCH -e /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Setophaga_pensylvanica.err #STDERR
+#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Setophaga_pensylvanica.out #STDOUT
+#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Setophaga_pensylvanica.err #STDERR
 
 #echos name of node
 echo `hostname`
 
 #load R module (auto loads singularity and run script)
 #module load R/3.5.2
-#Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Setophaga_pensylvanica
+#Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Setophaga_pensylvanica
 
 #load singularity and run R script using singularity
 module load singularity/3.0.2
-#singularity exec /home/CAM/cyoungflesh/R.sif Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Setophaga_pensylvanica
-singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Setophaga_pensylvanica
+#singularity exec /home/CAM/cyoungflesh/R.sif Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Setophaga_pensylvanica
+singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Setophaga_pensylvanica
 
 
 #displays amount of memory used
