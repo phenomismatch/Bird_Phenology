13acfa7a8065d67e33156adc43f9cbe31095a484
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-18 10:00:36 -0500

    update run script 4

--- Scripts/eBird_Nestwatch/4-IAR/species/4-Catharus_ustulatus.sh
+++ Scripts/eBird_Nestwatch/4-IAR/species/4-Catharus_ustulatus.sh
@@ -21,7 +21,9 @@ echo `hostname`
 
 #load singularity and run R script using singularity
 module load singularity/3.0.2
-singularity exec /home/CAM/cyoungflesh/R.sif Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Catharus_ustulatus
+#singularity exec /home/CAM/cyoungflesh/R.sif Rscript /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Catharus_ustulatus
+singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif /home/CAM/cyoungflesh/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-model.R Catharus_ustulatus
+
 
 #displays amount of memory used
 sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
