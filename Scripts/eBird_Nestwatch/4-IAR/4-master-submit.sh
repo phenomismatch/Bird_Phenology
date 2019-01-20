75cfd733afd6a0d19105ed73a0d897b6fd709eee
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-20 18:04:09 -0500

    prep for xanadu run

--- Scripts/eBird_Nestwatch/4-IAR/4-master-submit.sh
+++ Scripts/eBird_Nestwatch/4-IAR/4-master-submit.sh
@@ -5,8 +5,8 @@ do
   temp="${name%\"}"
   temp="${temp#\"}"
   sbatch species/4-$temp.sh
-#done < ../../../Data/IAR_species_list.txt
-done < ../../../Data/test_species_list.txt
+done < ../../../Data/IAR_species_list.txt
+#done < ../../../Data/test_species_list.txt
 
 DATE="2019-01-16"
 cp 4-IAR-model.R ../../../Data/Processed/IAR_output_$DATE/4-IAR-model-$DATE.R
