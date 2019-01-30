498841aebe196934651324e1adaaaa7f3c6bea81
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-30 11:12:42 -0500

    change file name

--- Scripts/eBird_Nestwatch/5-extract-arr-dates.R
+++ Scripts/eBird_Nestwatch/5-extract-arr-dates.R
@@ -102,7 +102,7 @@ for (i in 1:length(species))
 
 setwd(paste0(dir, 'Bird_Phenology/Data/Processed/'))
 
-saveRDS(out, file = paste0('master_arrival_', IAR_out_date, '.rds'))
+saveRDS(out, file = paste0('arrival_master', IAR_out_date, '.rds'))
 
 
 
