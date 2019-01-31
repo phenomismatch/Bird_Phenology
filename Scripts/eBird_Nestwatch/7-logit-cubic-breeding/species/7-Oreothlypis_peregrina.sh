f305921cfd730975d900026c6477ac6e56869e4e
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-30 21:26:17 -0500

    new species scripts

---
+++ Scripts/eBird_Nestwatch/7-logit-cubic-breeding/species/7-Oreothlypis_peregrina.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=lc-br-Oreothlypis_peregrina
+#SBATCH -N 1 #number of tasks
+#SBATCH -n 1 #number of nodes
+#SBATCH -c 4 #cpus
+#SBATCH --qos=general #queue (same as partition)
+#SBATCH --partition=general #partition - can also specify 'himem'
+#SBATCH --mem=8G #memory requested
+#SBATCH --mail-type=END #when to send email (on job completion)
+#SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
+#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_breeding_2019-01-30/lc-br-Oreothlypis_peregrina.out #STDOUT
+#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_breeding_2019-01-30/lc-br-Oreothlypis_peregrina.err #STDERR
+
+#echos name of node
+echo `hostname`
+
+#load singularity and run R script using singularity
+module load singularity/3.0.2
+singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/7-logit-cubic-breeding/7-logit-cubic-breeding.R Oreothlypis_peregrina
+
+#displays amount of memory used
+sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
