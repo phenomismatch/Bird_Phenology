6fa6becbcf5ea8f6d11b5cf9fe72729591342615
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-30 23:21:39 -0500

    add comments to script

--- Scripts/eBird_Nestwatch/singularity.sh
+++ Scripts/eBird_Nestwatch/singularity.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#argument is diretory of where to search for singularity error message
+#script finds all STDERR that has the singularity error +
+#print which node they occurred on +
+#creates a text file with lines to resbumit jobs +
+#removes all STDOUT SRDERR files associated with those jobs
+
 grep libseccomp.so.2 $1 -lR > tfile
 
 while read name
