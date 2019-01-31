9a15b0b9703f2310dfe8425d1c506cad9bb87648
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-01-30 21:06:11 -0500

    change submit scripts

--- Scripts/eBird_Nestwatch/4-IAR/species/4-Melospiza_lincolnii.sh
+++ Scripts/eBird_Nestwatch/4-IAR/species/4-Melospiza_lincolnii.sh
@@ -9,8 +9,8 @@
 #SBATCH --mem=8G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
-#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Melospiza_lincolnii.out #STDOUT
-#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16/iar-Melospiza_lincolnii.err #STDERR
+#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16-supp/iar-Melospiza_lincolnii.out #STDOUT
+#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-01-16-supp/iar-Melospiza_lincolnii.err #STDERR
 
 #echos name of node
 echo `hostname`
