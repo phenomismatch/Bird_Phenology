623202d1772d2ed08bd2819a5fc9b28040979ea2
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-02-02 12:56:50 -0500

    full xanadu run 2

--- Scripts/eBird_Nestwatch/2-logit-cubic/2-master-submit.sh
+++ Scripts/eBird_Nestwatch/2-logit-cubic/2-master-submit.sh
@@ -8,7 +8,7 @@ do
   temp="${name%\"}"
   temp="${temp#\"}"
   sbatch species/2-$temp.sh
-#done < ../../../Data/eBird_species_list.txt
-done < ../../../Data/test_species_list.txt
+done < ../../../Data/eBird_species_list.txt
+#done < ../../../Data/test_species_list.txt
 
 cp 2-logit-cubic.R ../../../Data/Processed/halfmax_species_$DATE/2-logit-cubic-$DATE.R
