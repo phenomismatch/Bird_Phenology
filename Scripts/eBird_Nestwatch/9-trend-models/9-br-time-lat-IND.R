8bae0395f53839ac916e92b4247fd7402e0f9655
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-02-10 10:34:08 -0500

    plotting for trend model results

--- Scripts/eBird_Nestwatch/9-trend-models/9-br-time-lat-IND.R
+++ Scripts/eBird_Nestwatch/9-trend-models/9-br-time-lat-IND.R
@@ -29,7 +29,7 @@ library(dplyr)
 library(ggplot2)
 library(rstan)
 library(MCMCvis)
-library(dggrid)
+library(dggridR)
 
 
 # import ARR/BR data ---------------------------------------------------------
@@ -86,6 +86,12 @@ mdf <- dplyr::select(master_data, species, cell, year, cell_lat, cell_lon,
 
 #do not merge with breeding data
 mdf3 <- filter(mdf, species == args)
+
+# #look at data availablity for species
+# nmdf <- mdf[which(!is.na(mdf$EB_HM_mean)),]
+# 
+# #maybe 35 species with 3 years of data - only a handful with more than that
+# plyr::count(nmdf, c('species', 'year'))
 
 #number codes for species
 #sp_num <- as.numeric(factor(mdf3$species))
@@ -265,10 +271,10 @@ fit <- rstan::stan(model_code = br_time_lat_ind,
                    cores = CHAINS,
                    pars = c('alpha', 'beta', 'mu_alpha', 
                             'alpha2', 'beta2',
-                            'sigma_alpha', 'sigma', 'y_true'),
-                   control = list(adapt_delta = DELTA,
-                                  max_treedepth = TREE_DEPTH,
-                                  stepsize = STEP_SIZE))
+                            'sigma_alpha', 'sigma', 'y_true'))#,
+                   # control = list(adapt_delta = DELTA,
+                   #                max_treedepth = TREE_DEPTH,
+                   #                stepsize = STEP_SIZE))
 run_time <- (proc.time() - tt[3]) / 60
 
 
@@ -321,6 +327,217 @@ cat(paste0('Number of tree exceeds: ', num_tree, ' \n'))
 cat(paste0('Number chains low BFMI: ', num_BFMI, ' \n'))
 print(fit)
 sink()
+
+
+
+# Extract posterior estimates ---------------------------------------------
+
+#extract median and sd estimates for x_true
+y_true_mean <- MCMCvis::MCMCpstr(fit, params = 'y_true', func = mean)[[1]]
+y_true_sd <- MCMCvis::MCMCpstr(fit, params = 'y_true', func = sd)[[1]]
+
+#extract slope estimates and CI
+beta_med <- MCMCvis::MCMCpstr(fit, params = 'beta', func = median)[[1]]
+beta_LCI <- MCMCvis::MCMCpstr(fit, params = 'beta', func = function(x) quantile(x, probs = c(0.025)))[[1]]
+beta_UCI <- MCMCvis::MCMCpstr(fit, params = 'beta', func = function(x) quantile(x, probs = c(0.975)))[[1]]
+beta_sd <- MCMCvis::MCMCpstr(fit, params = 'beta', func = sd)[[1]]
+
+
+# plots arr over time -----------------------------------------------------
+
+#for each cell:
+#X-AXIS = year
+#Y-AXIS = julian day
+#x_true with error bars
+#regression line
+
+#need true latent states
+DATA_PLOT <- data.frame(mean_y = y_true_mean,
+                        mean_y_l = y_true_mean - y_true_sd,
+                        mean_y_u = y_true_mean + y_true_sd,
+                        in_y = mdf3$EB_HM_mean,
+                        in_y_l = mdf3$EB_HM_mean - mdf3$EB_HM_sd,
+                        in_y_u = mdf3$EB_HM_mean + mdf3$EB_HM_sd,
+                        year = mdf3$year,
+                        cell = cell_mrg$cell,
+                        beta_med = beta_med,
+                        beta_LCI = beta_LCI,
+                        beta_UCI = beta_UCI,
+                        beta_sd = beta_sd)
+
+#plot for each cell
+for (i in 1:NROW(u_cell_mrg))
+{
+  #i <- 1
+  print(i)
+  
+  #model fit to plot
+  alpha_ch <- MCMCchains(fit, params = paste0('alpha\\[', 
+                                              u_cell_mrg$cell_num[i], '\\]'), ISB = FALSE)[,1]
+  beta_ch <- MCMCchains(fit, params = paste0('beta\\[', 
+                                             u_cell_mrg$cell_num[i], '\\]'), ISB = FALSE)[,1]
+  
+  sim_year <- seq(1, length(unique(mdf3$year)), length = 100)
+  sim_year_actual <- seq(min(mdf3$year), max(mdf3$year), length = 100)
+  
+  mf <- matrix(nrow = length(beta_ch), ncol = 100)
+  for (j in 1:length(sim_year))
+  {
+    mf[,j] <- alpha_ch + beta_ch * sim_year[j]
+  }
+  
+  med_mf <- apply(mf, 2, median)
+  LCI_mf <- apply(mf, 2, function(x) quantile(x, probs = 0.025))
+  UCI_mf <- apply(mf, 2, function(x) quantile(x, probs = 0.975))
+  
+  FIT_PLOT <- data.frame(MN = med_mf, 
+                         MN_YR = sim_year_actual,
+                         LCI = LCI_mf,
+                         UCI = UCI_mf)
+  
+  DATA_PLOT2 <- dplyr::filter(DATA_PLOT, cell == u_cell_mrg$cell[i])
+  
+  p <- ggplot(data = DATA_PLOT2, aes(year, mean_y)) +
+    geom_ribbon(data = FIT_PLOT,
+                aes(x = MN_YR, ymin = LCI, ymax = UCI),
+                fill = 'grey', alpha = 0.7,
+                inherit.aes = FALSE) +
+    geom_line(data = FIT_PLOT, aes(MN_YR, MN), color = 'red',
+              alpha = 0.9,
+              inherit.aes = FALSE,
+              size = 1.4) +
+    geom_point(data = DATA_PLOT2, aes(year - 0.1, mean_y), color = 'black',
+               inherit.aes = FALSE, size = 1.75, alpha = 0.5) +
+    geom_errorbar(data = DATA_PLOT2,
+                  aes(ymin = mean_y_l, ymax = mean_y_u, x = year - 0.1), width = 0.5,
+                  color = 'black', alpha = 0.4) +
+    geom_point(data = DATA_PLOT2, aes(year + 0.1, in_y), color = 'red',
+               inherit.aes = FALSE, size = 1.75, alpha = 0.3) +
+    geom_errorbar(data = DATA_PLOT2,
+                  aes(ymin = in_y_l, ymax = in_y_u, x = year + 0.1), width = 0.5,
+                  color = 'red', alpha = 0.3) +
+    theme_bw() +
+    #scale_x_discrete(limits = c(seq(18,30, by = 2))) +
+    xlab('Year') +
+    ylab('True BR') +
+    ylim(low = 80, high = 200) +
+    ggtitle(paste0(args, '; Cell: ', u_cell_mrg$cell[i])) +
+    theme(
+      plot.title = element_text(size = 12),
+      axis.text = element_text(size = 12),
+      axis.title = element_text(size = 14),
+      axis.title.y = element_text(margin = margin(t = 0, r = 5, b = 0, l = 0)),
+      axis.title.x = element_text(margin = margin(t = 5, r = 15, b = 0, l = 0)),
+      axis.ticks.length= unit(0.2, 'cm')) #length of axis tick
+  
+  assign(paste0('p_', i), p)
+}
+
+
+#save figs to pdfs
+setwd(paste0(dir, 'Bird_Phenology/Figures/breeding_trends'))
+pdf(paste0(args, '-plots-BR-time.pdf'), height = 6, width = 9, useDingbats = FALSE)
+
+counter <- 1
+for (i in 1:ceiling(NROW(u_cell_mrg)/4))
+{
+  if ((NROW(u_cell_mrg) - counter) >= 4)
+  {
+    gridExtra::grid.arrange(eval(as.name(paste0('p_', counter))), 
+                            eval(as.name(paste0('p_', counter + 1))), 
+                            eval(as.name(paste0('p_', counter + 2))), 
+                            eval(as.name(paste0('p_', counter + 3)))) 
+  } else {
+    if ((NROW(u_cell_mrg) - counter) == 3)
+    {
+      gridExtra::grid.arrange(eval(as.name(paste0('p_', counter))), 
+                              eval(as.name(paste0('p_', counter + 1))), 
+                              eval(as.name(paste0('p_', counter + 2)))) 
+    }
+    if ((NROW(u_cell_mrg) - counter) == 2)
+    {
+      gridExtra::grid.arrange(eval(as.name(paste0('p_', counter))), 
+                              eval(as.name(paste0('p_', counter + 1))))
+    }
+    if ((NROW(u_cell_mrg) - counter) == 1)
+    {
+      gridExtra::grid.arrange(eval(as.name(paste0('p_', counter))))
+    }
+  }
+  counter <- counter + 4
+}
+dev.off()
+
+
+
+
+# plot slope estimates on each cell on map --------------------------------------
+
+#slope in grey
+
+#make hexgrid
+hexgrid6 <- dggridR::dgconstruct(res = 6)
+
+#transform cells to grid
+cell_grid <- dggridR::dgcellstogrid(hexgrid6, u_cell_mrg$cell)
+cell_grid$cell <- as.numeric(cell_grid$cell)
+cell_centers <- dggridR::dgSEQNUM_to_GEO(hexgrid6, u_cell_mrg$cell)
+ll_df <- data.frame(cell = u_cell_mrg$cell, 
+                    lon_deg = cell_centers$lon_deg, 
+                    lat_deg = cell_centers$lat_deg)
+
+#load maps
+usamap <- data.frame(maps::map("world", "USA", plot = FALSE)[c("x", "y")])
+canadamap <- data.frame(maps::map("world", "Canada", plot = FALSE)[c("x", "y")])
+mexicomap <- data.frame(maps::map("world", "Mexico", plot = FALSE)[c("x", "y")])
+
+
+#determine min/max for plotting
+f_rng <- range(beta_med)
+MIN <- round(min(f_rng), 3)
+MAX <- round(max(f_rng), 3)
+
+
+
+#merge hex spatial data with HM data
+to_plt <- dplyr::inner_join(DATA_PLOT, cell_grid, by = 'cell')
+to_plt2 <- dplyr::inner_join(to_plt, ll_df, by = 'cell')
+
+#plot
+fp <- ggplot() +
+  geom_path(data = usamap, 
+            aes(x = x, y = y), color = 'black') + 
+  geom_path(data = canadamap, 
+            aes(x = x, y = y), color = 'black') + 
+  geom_path(data = mexicomap, 
+            aes(x = x, y = y), color = 'black') + 
+  # geom_polygon(data = nrng.df, 
+  #           aes(x = long, y = lat, group=group), fill = 'green', alpha = 0.4) + 
+  # geom_polygon(data = nrng_rm.df, 
+  #              aes(x = long, y = lat, group=group), fill = 'orange', alpha = 0.4) + 
+  coord_map("ortho", orientation = c(35, -80, 0), 
+            xlim = c(-100, -55), ylim = c(25, 66)) + 
+  geom_polygon(data = to_plt2, aes(x = long, y = lat, group = group, fill = beta_med), 
+               alpha = 0.4) +
+  geom_path(data = to_plt2, aes(x = long, y = lat, group = group), 
+            alpha = 0.4, color = 'black') + 
+  scale_fill_gradientn(colors = c('red', 'blue'),
+                       limits = c(MIN, MAX)) +
+  labs(fill = 'Slope estimate') +
+  annotate('text', x = to_plt2$lon_deg, y = to_plt2$lat_deg + 0.5, 
+           label = round(to_plt2$beta_med, digits = 2), col = 'black', alpha = 0.2,
+           size = 3) +
+  annotate('text', x = to_plt2$lon_deg, y = to_plt2$lat_deg - 0.5, 
+           label = round(to_plt2$beta_sd, digits = 2), 
+           col = 'white', alpha = 0.3,
+           size = 2.5) +
+  ggtitle(paste0(args, '-slope-BR-time')) +
+  theme_bw() +
+  xlab('Longitude') +
+  ylab('Latitude')
+
+setwd(paste0(dir, 'Bird_Phenology/Figures/breeding_trends'))
+ggsave(plot = fp, filename = paste0(args, '-slope-map-BR-time.pdf'))
 
 
 
