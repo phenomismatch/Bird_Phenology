ab1c7f4c94b36d61405c0ec4621d5e0ca7e7f0cc
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-02-13 12:53:09 -0500

    Change IAR species list

--- Scripts/eBird_Nestwatch/4-IAR/4-master-submit.sh
+++ Scripts/eBird_Nestwatch/4-IAR/4-master-submit.sh
@@ -8,7 +8,7 @@ do
   temp="${name%\"}"
   temp="${temp#\"}"
   sbatch species/4-$temp.sh
-done < ../../../Data/IAR_species_list.txt
+done < ../../../Data/IAR_species_list-2019-02-02.txt
 #done < ../../../Data/test_species_list.txt
 
 cp 4-IAR-model.R ../../../Data/Processed/IAR_output_$DATE/4-IAR-model-$DATE.R
