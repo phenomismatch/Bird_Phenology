1fa00780a62bddfcad91922553c2bd775c7068bc
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-02-17 23:42:41 -0500

    create 3b batch scripts

--- Scripts/eBird_Nestwatch/3b-extract-temps/3b-create-batch-scripts.sh
+++ Scripts/eBird_Nestwatch/3b-extract-temps/3b-create-batch-scripts.sh
@@ -7,10 +7,10 @@ do
 #SBATCH --job-name=daymet-$year
 #SBATCH -N 1 #number of tasks
 #SBATCH -n 1 #number of nodes
-#SBATCH -c 10 #cpus
+#SBATCH -c 6 #cpus
 #SBATCH --qos=general #queue (same as partition)
 #SBATCH --partition=general #partition - can also specify 'himem'
-#SBATCH --mem=10G #memory requested
+#SBATCH --mem=30G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
 #SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/daymet/daymet-$year.out #STDOUT
