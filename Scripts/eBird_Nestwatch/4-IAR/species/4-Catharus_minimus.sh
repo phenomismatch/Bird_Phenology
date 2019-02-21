46aac1d941601b62f757e02fbb70f0c727f90c52
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-02-21 17:25:04 -0500

    update for xanadu

--- Scripts/eBird_Nestwatch/4-IAR/species/4-Catharus_minimus.sh
+++ Scripts/eBird_Nestwatch/4-IAR/species/4-Catharus_minimus.sh
@@ -9,8 +9,8 @@
 #SBATCH --mem=8G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
-#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-02-13/Catharus_minimus-iar3.out #STDOUT
-#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-02-13/Catharus_minimus-iar3.err #STDERR
+#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-02-21/Catharus_minimus-iar.out #STDOUT
+#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2019-02-21/Catharus_minimus-iar.err #STDERR
 
 #echos name of node
 echo `hostname`
