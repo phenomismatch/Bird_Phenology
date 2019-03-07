733a3734fe6943ba01d309d4db05a1540eab42ab
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-03-07 11:06:52 -0500

    add additional diagnostics

--- Scripts/eBird_Nestwatch/2-logit-cubic/model_tests/2-logit_cubic-model-test.R
+++ Scripts/eBird_Nestwatch/2-logit-cubic/model_tests/2-logit_cubic-model-test.R
@@ -2,9 +2,10 @@
 #Determine complexity of polynomial models
 #
 #calculate WAIC and SSE
+#produce diagnostic plots with PPC on binned data
 #########################
 
-#Species, years, cells investigated
+#Species, years, cells investigated (inital)
 #Agelaius phoeniceus - 2014 - 536
 #Agelaius phoeniceus - 2014 - 538
 #Agelaius phoeniceus - 2014 - 564
@@ -30,7 +31,7 @@ RUN_DATE <- '2019-02-02'
 # model settings ----------------------------------------------------------
 
 #number of iterations each model should be run
-ITER <- 500
+ITER <- 750
 #ITER <- 10
 CHAINS <- 4
 
@@ -235,13 +236,22 @@ if (NROW(nrng@data) > 0)
 
 # process data ------------------------------------------------------------
 
-cells <- c(536, 538, 564, 566)
+#cells <- c(536, 538, 564, 566)
 ncell <- length(cells)
 
 years <- min(spdata2$year):max(spdata2$year)
+#years <- 2014
 nyr <- length(years)
 
 
+formulas <- list(f2 = 'detect ~ sjday + sjday2 + shr',
+                 f3 = 'detect ~ sjday + sjday2 + sjday3 + shr',
+                 f4 = 'detect ~ sjday + sjday2 + sjday3 + sjday4 + shr',
+                 f5 = 'detect ~ sjday + sjday2 + sjday3 + sjday4 + sjday5 + shr',
+                 f6 = 'detect ~ sjday + sjday2 + sjday3 + sjday4 + sjday5 + sjday6 + shr')
+
+#number of models
+nm <- length(formulas)
 
 # fit logit cubic ---------------------------------------------------------
 
@@ -251,11 +261,13 @@ nyr <- length(years)
 #                          year = rep(years, each = ncell), 
 #                          cell = rep(cells, nyr), 
 
-t_mat <- matrix(data = NA, nrow = 4, ncol = ((ITER/2)*CHAINS))
-colnames(t_mat) <- paste0('iter_', 1:((ITER/2)*CHAINS))
-halfmax_df <- data.frame(species = args, 
-                         year = 2014, 
-                         cell = cells, 
+#t_mat <- matrix(data = NA, nrow = ncell * nyr * nm, ncol = ((ITER/2)*CHAINS))
+#colnames(t_mat) <- paste0('iter_', 1:((ITER/2)*CHAINS))
+
+halfmax_df <- data.frame(species = rep(args, times = (ncell * nyr * nm)), 
+                         year = NA, 
+                         cell = NA,
+                         poly = NA,
                          max_Rhat = NA,
                          min_neff = NA,
                          num_diverge = NA,
@@ -270,22 +282,10 @@ halfmax_df <- data.frame(species = args,
                          njd1 = NA,
                          njd0 = NA,
                          njd0i = NA,
-                         hf_mean_m3 = NA,
-                         hf_sd_m3 = NA,
-                         hf_mean_m4 = NA,
-                         hf_sd_m4 = NA,
-                         hf_mean_m5 = NA,
-                         hf_sd_m5 = NA,
-                         hf_mean_m6 = NA,
-                         hf_sd_m6 = NA,
-                         waic_3 = NA,
-                         waic_se_3 = NA,
-                         waic_4 = NA,
-                         waic_se_4 = NA,
-                         waic_5 = NA,
-                         waic_se_5 = NA,
-                         waic_6 = NA,
-                         waic_se_6 = NA,
+                         hf_mean = NA,
+                         hf_sd = NA,
+                         waic = NA,
+                         waic_se = NA,
                          ppc_b1 = NA,
                          ppc_b2 = NA,
                          ppc_b3 = NA,
@@ -331,24 +331,16 @@ spdata2$sjday7 <- spdata2$sjday^7
 #                  f7 = 'detect ~ ssjday + ssjday2 + ssjday3 + ssjday4 + ssjday5 + ssjday6 + ssjday7 + shr')
 
 
-formulas <- list(f3 = 'detect ~ sjday + sjday2 + sjday3 + shr',
-                 f4 = 'detect ~ sjday + sjday2 + sjday3 + sjday4 + shr',
-                 f5 = 'detect ~ sjday + sjday2 + sjday3 + sjday4 + sjday5 + shr',
-                 f6 = 'detect ~ sjday + sjday2 + sjday3 + sjday4 + sjday5 + sjday6 + shr')
-
-
-
-
 #loop through each species, year, cell and extract half-max parameter
 counter <- 1
-#for (j in 1:nyr)
-#{
-  j <- 13
+for (j in 1:nyr)
+{
+  #j <- 1
   yspdata <- spdata2[which(spdata2$year == years[j]), ]
   
   for (k in 1:ncell)
   {
-    #k <- 33
+    #k <- 1
     print(paste0('species: ', args, ', year: ', j, ', cell: ', k))
     
     cyspdata <- yspdata[which(yspdata$cell == cells[k]), ]
@@ -378,13 +370,6 @@ counter <- 1
       n0i <- 0
     }
     
-    halfmax_df$n1[counter] <- n1
-    halfmax_df$n1W[counter] <- n1W
-    halfmax_df$n0[counter] <- n0
-    halfmax_df$n0i[counter] <- n0i
-    halfmax_df$njd1[counter] <- njd1
-    halfmax_df$njd0[counter] <- njd0
-    halfmax_df$njd0i[counter] <- njd0i
     
     #defaults for rstanarm are 0.95 and 15
     DELTA <- 0.95
@@ -420,11 +405,24 @@ counter <- 1
     #                       ssjday7 = predictDays7, shr = 0)
     
     
-    #if (n1 > 29 & n1W < (n1/50) & n0 > 29 & njd0i > 29 & njd1 > 19)
-    #{
+    if (n1 > 29 & n1W < (n1/50) & n0 > 29 & njd0i > 29 & njd1 > 19)
+    {
       for (m in 1:length(formulas))
       {
-        #m <- 2
+        #m <- 1
+        
+        halfmax_df$year[counter] <- years[j]
+        halfmax_df$cell[counter] <- cells[k]
+        halfmax_df$poly[counter] <- m + 1
+        
+        halfmax_df$n1[counter] <- n1
+        halfmax_df$n1W[counter] <- n1W
+        halfmax_df$n0[counter] <- n0
+        halfmax_df$n0i[counter] <- n0i
+        halfmax_df$njd1[counter] <- njd1
+        halfmax_df$njd0[counter] <- njd0
+        halfmax_df$njd0i[counter] <- njd0i
+        
         tfit <- rstanarm::stan_glm(formulas[[m]], 
                                    data = cyspdata,
                                    family = binomial(link = "logit"),
@@ -435,14 +433,19 @@ counter <- 1
                                    adapt_delta = DELTA,
                                    control = list(max_treedepth = TREE_DEPTH))
       
-        nm <- paste0(args, '_', years[j], '_', cells[k], '-poly_', m + 2)
-        assign(nm, tfit)
+        name <- paste0(args, '_', years[j], '_', cells[k], '-poly_', m + 1)
+        assign(name, tfit)
         
         #calculate diagnostics
-        #num_diverge <- rstan::get_num_divergent(fit2$stanfit)
-        #num_tree <- rstan::get_num_max_treedepth(fit2$stanfit)
-        #num_BFMI <- length(rstan::get_low_bfmi_chains(fit2$stanfit))
-      
+        halfmax_df$num_diverge[counter] <- rstan::get_num_divergent(tfit$stanfit)
+        halfmax_df$num_tree[counter] <- rstan::get_num_max_treedepth(tfit$stanfit)
+        halfmax_df$num_BFMI[counter] <- length(rstan::get_low_bfmi_chains(tfit$stanfit))
+        halfmax_df$delta[counter] <- DELTA
+        halfmax_df$tree_depth[counter] <- TREE_DEPTH
+        halfmax_df$max_Rhat[counter] <- round(max(summary(tfit)[, "Rhat"]), 2)
+        halfmax_df$min_neff[counter] <- min(summary(tfit)[, "n_eff"])
+        
+        
         #predict response
         dfit <- rstanarm::posterior_linpred(tfit, newdata = newdata, transform = T)
         halfmax_fit <- rep(NA, ((ITER/2)*CHAINS))
@@ -460,6 +463,65 @@ counter <- 1
         #assign bin numbers to each point
         bin_number <- 1 + cyspdata$day %/% 20
 
+        ########################
+        #PLOT MODEL FIT AND DATA
+        
+        setwd('~/Desktop/Agelaius_phoeniceus_tests')
+        cyspdata2 <- cyspdata
+        
+        #summary(fit2)
+        mn_dfit <- apply(dfit, 2, mean)
+        LCI_dfit <- apply(dfit, 2, function(x) quantile(x, probs = 0.025))
+        UCI_dfit <- apply(dfit, 2, function(x) quantile(x, probs = 0.975))
+        mn_hm <- mean(halfmax_fit)
+        LCI_hm <- quantile(halfmax_fit, probs = 0.025)
+        UCI_hm <- quantile(halfmax_fit, probs = 0.975)
+        
+        pdf(paste0(args, '_', years[j], '_', cells[k], '-poly_', m+1, '_arrival.pdf'))
+        plot(predictDays, UCI_dfit, type = 'l', col = 'red', lty = 2, lwd = 3,
+             ylim = c(-(max(UCI_dfit)/5), max(UCI_dfit)),
+             main = paste0(args, ' - ', years[j], ' - ', cells[k], '- poly ', m + 1),
+             xlab = 'Julian Day', ylab = 'Detection Probability')
+        lines(predictDays, LCI_dfit, col = 'red', lty = 2, lwd = 3)
+        lines(predictDays, mn_dfit, lwd = 3)
+        cyspdata2$detect[which(cyspdata2$detect == 1)] <- max(UCI_dfit)
+        points(cyspdata2$day, cyspdata2$detect, col = rgb(0,0,0,0.25))
+        
+        ds <- sort(unique(cyspdata$day))
+        prop_d <- rep(NA, length(ds))
+        for (p in 1:length(ds))
+        {
+          #p <- 2
+          td <- filter(cyspdata, day == ds[p])
+          prop_d[p] <- mean(td$detect)
+        }
+        
+        ma <- function(x, n = 5)
+        {
+          stats::filter(x, rep(1/n, n), sides = 2)
+        }
+        ma_prop_d <- ma(prop_d) * max(UCI_dfit)
+        
+        lines(ds, ma_prop_d, col = rgb(0.5,0.5,0.5,0.9), lwd = 2)
+        
+        segments(x0 = mn_hm, x1 = mn_hm, y0 = 0, y1 = 1, col = rgb(0,0,1,0.5), lwd = 3)
+        segments(x0 = LCI_hm, x1 = LCI_hm, y0 = 0, y1 = 1, col = rgb(0,0,1,0.5), lwd = 3, lty = 2)
+        segments(x0 = UCI_hm, x1 = UCI_hm, y0 = 0, y1 = 1, col = rgb(0,0,1,0.5), lwd = 3, lty = 2)
+
+        rect(xleft = 0, ybottom = 0, xright = 20, ytop = max(UCI_dfit),
+             col = rgb(0,1,1,0.2), border = FALSE)
+        rect(xleft = 40, ybottom = 0, xright = 60, ytop = max(UCI_dfit),
+             col = rgb(0,1,1,0.2), border = FALSE)
+        rect(xleft = 80, ybottom = 0, xright = 100, ytop = max(UCI_dfit),
+             col = rgb(0,1,1,0.2), border = FALSE)
+        rect(xleft = 120, ybottom = 0, xright = 140, ytop = max(UCI_dfit),
+             col = rgb(0,1,1,0.2), border = FALSE)
+        rect(xleft = 160, ybottom = 0, xright = 180, ytop = max(UCI_dfit),
+             col = rgb(0,1,1,0.2), border = FALSE)
+        legend('topleft',
+               legend = c('Cubic fit', 'CI fit', 'Half max', 'CI HM', '5d MA'),
+               col = c('black', 'red', rgb(0,0,1,0.5), rgb(0,0,1,0.5), rgb(0.5,0.5,0.5,0.7)),
+               lty = c(1,2,1,2), lwd = c(2,2,2,2), cex = 1.3)
         for (bin in 1:10) 
         {
           #bin <- 1
@@ -476,60 +538,33 @@ counter <- 1
           # sum(binpredict == truenumber) / 1000
           # sum(binpredict > truenumber) / 1000
           # sum(binpredict < truenumber) / 1000
+          
+          h_plt <- function()
+          {
+            hist(binpredict, xaxt = 'n', yaxt = 'n', main = NULL,
+                 xlab = NULL, ylab = NULL)
+            abline(v = truenumber, col = 'red', lwd = 3)
+          }
+          Hmisc::subplot(h_plt, x = ((bin * 20) - 8), y = -(max(UCI_dfit) / 8), size = c(0.5, 0.5))
         }
         
-        
-        ########################
-        #PLOT MODEL FIT AND DATA
-        
-        setwd('~/Desktop')
-        cyspdata2 <- cyspdata
-        
-        #summary(fit2)
-        mn_dfit <- apply(dfit, 2, mean)
-        LCI_dfit <- apply(dfit, 2, function(x) quantile(x, probs = 0.025))
-        UCI_dfit <- apply(dfit, 2, function(x) quantile(x, probs = 0.975))
-        mn_hm <- mean(halfmax_fit)
-        LCI_hm <- quantile(halfmax_fit, probs = 0.025)
-        UCI_hm <- quantile(halfmax_fit, probs = 0.975)
-        
-        pdf(paste0(args, '_', years[j], '_', cells[k], '-model_num_', m, '_arrival.pdf'))
-        plot(predictDays, UCI_dfit, type = 'l', col = 'red', lty = 2, lwd = 2,
-             ylim = c(0, max(UCI_dfit)),
-             main = paste0(args, ' - ', years[j], ' - ', cells[k]),
-             xlab = 'Julian Day', ylab = 'Detection Probability')
-        lines(predictDays, LCI_dfit, col = 'red', lty = 2, lwd = 2)
-        lines(predictDays, mn_dfit, lwd = 2)
-        cyspdata2$detect[which(cyspdata2$detect == 1)] <- max(UCI_dfit)
-        points(cyspdata2$day, cyspdata2$detect, col = rgb(0,0,0,0.25))
-        abline(v = mn_hm, col = rgb(0,0,1,0.5), lwd = 2)
-        abline(v = LCI_hm, col = rgb(0,0,1,0.5), lwd = 2, lty = 2)
-        abline(v = UCI_hm, col = rgb(0,0,1,0.5), lwd = 2, lty = 2)
-        legend('topleft',
-               legend = c('Cubic fit', 'CI fit', 'Half max', 'CI HM'),
-               col = c('black', 'red', rgb(0,0,1,0.5), rgb(0,0,1,0.5)),
-               lty = c(1,2,1,2), lwd = c(2,2,2,2), cex = 1.3)
         dev.off()
         ########################
         
         #halfax mean and sd
-        ind_mean <- grep(paste0('hf_mean_m', m), colnames(halfmax_df))
-        ind_sd <- grep(paste0('hf_sd_m', m), colnames(halfmax_df))
-        
-        halfmax_df[counter, ind_mean] <- mean(halfmax_fit)
-        halfmax_df[counter, ind_sd] <- sd(halfmax_fit)
-        
+        halfmax_df[counter, 'hf_mean'] <- mean(halfmax_fit)
+        halfmax_df[counter, 'hf_sd'] <- sd(halfmax_fit)
         
         #waic and se
-        ind_waic <- grep(paste0('waic_', m), colnames(halfmax_df))
-        ind_waic_se <- grep(paste0('waic_se_', m), colnames(halfmax_df))
+        halfmax_df[counter, 'waic'] <- waic(tfit)$estimates[3, 1]
+        halfmax_df[counter, 'waic_se'] <- waic(tfit)$estimates[3, 2]
         
-        halfmax_df[counter, ind_waic] <- waic(tfit)$estimates[3, 1]
-        halfmax_df[counter, ind_waic_se] <- waic(tfit)$estimates[3, 2]
+        counter <- counter + 1
       }
-    
-    #}
-    counter <- counter + 1
+    }
   } #k
-#} #j
+} #j
 
+
+saveRDS(halfmax_df, 'Agelaius_phoeniceus_poly_tests.rds')
+  
