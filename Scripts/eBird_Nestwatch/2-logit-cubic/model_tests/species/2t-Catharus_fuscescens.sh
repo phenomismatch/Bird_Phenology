557ed2da8a5058c00ee23c3230ea1c48f57da1a3
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-03-09 18:33:16 -0500

    new 2t scripts

--- Scripts/eBird_Nestwatch/2-logit-cubic/model_tests/species/2t-Catharus_fuscescens.sh
+++ Scripts/eBird_Nestwatch/2-logit-cubic/model_tests/species/2t-Catharus_fuscescens.sh
@@ -17,7 +17,7 @@ echo `hostname`
 
 #load singularity and run R script using singularity
 module load singularity/3.0.2
-singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/2-logit-cubic/2t-logit-cubic.R Catharus_fuscescens
+singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/2-logit-cubic/model_tests/2t-logit-cubic.R Catharus_fuscescens
 
 #displays amount of memory used
 sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
