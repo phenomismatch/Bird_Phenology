875f37ee23dcdb653874c697db973fb1b7cd44cf
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-03-19 13:30:02 -0400

    add processing

--- Scripts/eBird_Nestwatch/MAPS_exploration.R
+++ Scripts/eBird_Nestwatch/MAPS_exploration.R
@@ -16,20 +16,20 @@
 #--------------------------------#
 #distirbution of hatch year birds caught over the season
 
-maps_f <- dplyr::filter(maps_data, true_age == 0)
-plyr::count(maps_f, 'Sci_name')
+maps_young <- dplyr::filter(maps_data, true_age == 0)
+plyr::count(maps_young, 'Sci_name')
 
-oc_0 <- filter(maps_f, sci_name == 'Oreothlypis celata')
+oc_0 <- filter(maps_young, sci_name == 'Oreothlypis celata')
 plyr::count(oc_0, c('Cell', 'Year'))
 tt <- dplyr::filter(oc_0, Cell == 444, Year == 2011)
 
-plot(density(tt$Jday))
+plot(density(tt$day))
 
 #--------------------------------#
 
 
 #--------------------------------#
-#logistic model for presence of brood patch
+#logistic model for presence of brood patch (onset of breeding)
 
 maps_adults <- dplyr::filter(maps_data, age %in% c('1', '5', '6', '7', '8'))
 
@@ -41,8 +41,9 @@ C_ustulatus <- dplyr::filter(maps_adults, sci_name == 'Catharus ustulatus')
 #BP 2-4
 #CP 1-3
 
-cu_f <- dplyr::filter(C_ustulatus, sex == 'F')
-cu_m <- dplyr::filter(C_ustulatus, sex == 'M')
+#only captures that recorded brood patch/cloacal protuberance
+cu_f <- dplyr::filter(C_ustulatus, sex == 'F', brood_patch %in% c(0:5))
+cu_m <- dplyr::filter(C_ustulatus, sex == 'M', cloacal_pro %in% c(0:3))
 
 #add breeder col
 C_ustulatus$br <- NA
@@ -61,17 +62,19 @@ plot(density(cu_f[which(cu_f$brood_patch <= 2 | cu_f$brood_patch == 5),]$day))
 lines(density(cu_f[which(cu_f$brood_patch > 2 & cu_f$brood_patch < 5),]$day), 
       col = 'red')
 
-cu_f[which(cu_f$brood_patch > 2 & cu_f$brood_patch < 5),]$br <- 1
-cu_f[which(cu_f$brood_patch <= 2 | cu_f$brood_patch == 5),]$br <- 0
+cu_f[which(cu_f$brood_patch > 2 & cu_f$brood_patch < 5),]$bp <- 1
+cu_f[which(cu_f$brood_patch <= 2 | cu_f$brood_patch == 5),]$bp <- 0
 
 #DATA <- dplyr::filter(cu_f, Jday < DAY, Jday > 50)
 DATA <- cu_f
 
-DATA$jday <- DATA$day
-DATA$jday2 <- DATA$day^2
-DATA$jday3 <- DATA$day^3
-
 plyr::count(cu_f, c('Cell', 'Year'))
+
+
+
+
+#cyspdata
+
 
 
 library(rstanarm)
@@ -83,7 +86,7 @@ CHAINS <- 3
 DELTA <- 0.95
 TREE_DEPTH <- 15
 
-fit2 <- rstanarm::stan_gamm4(br ~ s(jday), 
+fit2 <- rstanarm::stan_gamm4(bp ~ s(day), 
                              data = cyspdata,
                              family = binomial(link = "logit"),
                              algorithm = 'sampling',
@@ -92,7 +95,6 @@ fit2 <- rstanarm::stan_gamm4(br ~ s(jday),
                              cores = CHAINS,
                              adapt_delta = DELTA,
                              control = list(max_treedepth = TREE_DEPTH))
-
 
 #calculate diagnostics
 num_diverge <- rstan::get_num_divergent(fit2$stanfit)
@@ -105,7 +107,7 @@ while (sum(c(num_diverge, num_tree, num_BFMI)) > 0 & DELTA <= 0.98)
   DELTA <- DELTA + 0.01
   TREE_DEPTH <- TREE_DEPTH + 1
   
-  fit2 <- rstanarm::stan_gamm4(detect ~ s(jday) + shr,
+  fit2 <- rstanarm::stan_gamm4(bp ~ s(day),
                                data = cyspdata,
                                family = binomial(link = "logit"),
                                algorithm = 'sampling',
@@ -120,15 +122,9 @@ while (sum(c(num_diverge, num_tree, num_BFMI)) > 0 & DELTA <= 0.98)
   num_BFMI <- length(rstan::get_low_bfmi_chains(fit2$stanfit))
 }
 
-halfmax_df$num_diverge[counter] <- num_diverge
-halfmax_df$num_tree[counter] <- num_tree
-halfmax_df$num_BFMI[counter] <- num_BFMI
-halfmax_df$delta[counter] <- DELTA
-halfmax_df$tree_depth[counter] <- TREE_DEPTH
-
 #generate predict data
-predictDays <- range(cyspdata$jday)[1]:range(cyspdata$jday)[2]
-newdata <- data.frame(jday = predictDays, shr = 0)
+predictDays <- range(cyspdata$day)[1]:range(cyspdata$day)[2]
+newdata <- data.frame(day = predictDays)
 
 #predict response
 dfit <- rstanarm::posterior_linpred(fit2, newdata = newdata, transform = T)
@@ -141,8 +137,11 @@ for (L in 1:((ITER/2)*CHAINS))
   halfmax_fit[L] <- predictDays[min(which(rowL > (max(rowL)/2)))]
 }
 
+
 ########################
 #PLOT MODEL FIT AND DATA
+
+cyspdata2 <- cyspdata
 
 #summary(fit2)
 mn_dfit <- apply(dfit, 2, mean)
@@ -152,15 +151,15 @@ mn_hm <- mean(halfmax_fit)
 LCI_hm <- quantile(halfmax_fit, probs = 0.025)
 UCI_hm <- quantile(halfmax_fit, probs = 0.975)
 
-pdf(paste0(args, '_', years[j], '_', cells[k], '_arrival.pdf'))
+#pdf(paste0(args, '_', years[j], '_', cells[k], '_arrival.pdf'))
 plot(predictDays, UCI_dfit, type = 'l', col = 'red', lty = 2, lwd = 2,
      ylim = c(0, max(UCI_dfit)),
      main = paste0(args, ' - ', years[j], ' - ', cells[k]),
-     xlab = 'Julian Day', ylab = 'Detection Probability')
+     xlab = 'Julian Day', ylab = 'Probability')
 lines(predictDays, LCI_dfit, col = 'red', lty = 2, lwd = 2)
 lines(predictDays, mn_dfit, lwd = 2)
-cyspdata$detect[which(cyspdata$detect == 1)] <- max(UCI_dfit)
-points(cyspdata$day, cyspdata$detect, col = rgb(0,0,0,0.25))
+cyspdata2$bp[which(cyspdata2$bp == 1)] <- max(UCI_dfit)
+points(cyspdata2$day, cyspdata2$bp, col = rgb(0,0,0,0.25))
 abline(v = mn_hm, col = rgb(0,0,1,0.5), lwd = 2)
 abline(v = LCI_hm, col = rgb(0,0,1,0.5), lwd = 2, lty = 2)
 abline(v = UCI_hm, col = rgb(0,0,1,0.5), lwd = 2, lty = 2)
@@ -168,60 +167,74 @@ legend('topleft',
        legend = c('Cubic fit', 'CI fit', 'Half max', 'CI HM'),
        col = c('black', 'red', rgb(0,0,1,0.5), rgb(0,0,1,0.5)),
        lty = c(1,2,1,2), lwd = c(2,2,2,2), cex = 1.3)
-dev.off()
+#dev.off()
+
+########################
 
 
 
 
 
+# phenology as it relates to age ------------------------------------------
 
-# phenology as it related to age ------------------------------------------
-
-# jfit <- lm(MAPS_age$Jday ~ MAPS_age$True_age)
+# jfit <- lm(maps_adults$day ~ maps_adults$true_age)
 # summary(jfit)
 # 
-# plot(MAPS_age$True_age, MAPS_age$Jday, pch = '.', col = rgb(0,0,0, 0.5))
+# plot(maps_adults$true_age, maps_adults$day, pch = '.', col = rgb(0,0,0, 0.5))
 # abline(jfit, col = 'red')
-# a1 <- dplyr::filter(MAPS_age, True_age == 1)
-# a2 <- dplyr::filter(MAPS_age, True_age == 2)
-# a3 <- dplyr::filter(MAPS_age, True_age == 3)
-# a4 <- dplyr::filter(MAPS_age, True_age == 4)
-# a5 <- dplyr::filter(MAPS_age, True_age == 5)
-# plot(density(a1$Jday))
-# lines(density(a2$Jday))
-# lines(density(a3$Jday))
-# lines(density(a4$Jday))
-# lines(density(a5$Jday))
+# a1 <- dplyr::filter(maps_adults, true_age == 1)
+# a2 <- dplyr::filter(maps_adults, true_age == 2)
+# a3 <- dplyr::filter(maps_adults, true_age == 3)
+# a4 <- dplyr::filter(maps_adults, true_age == 4)
+# a5 <- dplyr::filter(maps_adults, true_age == 5)
+# 
+# plot(density(a1$day))
+# lines(density(a2$day))
+# lines(density(a3$day))
+# lines(density(a4$day))
+# lines(density(a5$day))
+# 
+# hist(maps_adults$true_age)
+
+#filter sex, fat, weight, wing chord and plot densities
 
 
 
-#*breeding timing
-#Each species/cell/year:
-#maybe each capture different row, 1 for brood patch/CP, 0 if not -> fit a logistic to this for each cell/year?
-#y[i] ~ bern(p[i])
-#logit(p[i]) = alpha + beta * DAY[i]
-
-#*phenology and age
+#*phenology and age - could fit age as random effect in gam model above using rstanarm
 #could fit logistic regression with age as random effect -> each age class would have different beta param
 #for each cell/year: predict start of breeding for each age class (age 1, age 2, age 3+)
 #calculate derived qty - difference in halfmax estimates
 #y[i] ~ bern(p[i])
 #logit(p[i]) = alpha[id[i]] + beta[id[i]] * DAY[i]
 
-#*difference in arrival between males and females - how this maps onto phlogeny/traits
+
+
+#*for a given species, number of birds caught per hour in each cell
+#*fit gam and find halfmax?
+#*add params: age, sex, fat, weight, wing chord
+#*in generating prediction data, need to hold all vars constant with the exception of one of interest
+#e.g., vary age, mean fat, mean weight, mean wing chord 
+
+
+# difference in arrival based on traits -----------------------------------
+
+#does fat, weight, sex, wing chord, age explain differences in arrival timing?
+
+
+
 
 
 
 
 # other metric changes with age -------------------------------------------
 
-
-#maybe standardized by wing chord
-
 #*fat content and age - Fat[i] ~ alpha[id[i]] + beta1[id[i]] * Age[i] + beta2[id[i]] * Day[i] + beta3[id[i]] * Day[i]^2
 plot(MAPS_age$True_age, MAPS_age$Fat_content, col = rgb(0,0,0, 0.5))
 plot(MAPS_age$Jday, MAPS_age$Fat_content, col = rgb(0,0,0, 0.5))
 summary(lm(Fat_content ~ True_age + poly(Jday, 2, raw = TRUE), data = MAPS_age))
+
+
+#maybe standardize weight by wing chord
 
 #*Weight and age - Weight[i] ~ alpha[id[i]] + beta1[id[i]] * Age[i] + beta2[id[i]] * Day[i] + beta3[id[i]] * Day[i]^2
 plot(MAPS_age$True_age, MAPS_age$Weight, col = rgb(0,0,0, 0.5))
@@ -231,6 +244,22 @@ summary(lm(Weight ~ True_age + poly(Jday, 2, raw = TRUE), data = MAPS_age))
 
 
 
+# are there years where birds were heavier or lighter? --------------------
+
+
+#*is weight changing over time?
+#*is wing chord changing over time?
+
+
+
+
 # has age distribution of birds changed over time? ------------------------
 
 #more older birds over time? more younger birds over time?
+
+
+
+
+# how does weight of individual birds change (inter- and intra-annual) -----
+
+
