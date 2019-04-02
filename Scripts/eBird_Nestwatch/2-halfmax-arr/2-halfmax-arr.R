72e66232db135b95fd65ae22caabe3d51f7330e2
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-04-01 20:17:43 -0400

    change dir back

--- Scripts/eBird_Nestwatch/2-halfmax-arr/2-halfmax-arr.R
+++ Scripts/eBird_Nestwatch/2-halfmax-arr/2-halfmax-arr.R
@@ -269,11 +269,11 @@ halfmax_df <- data.frame(species = args,
 
 
 #create dir for figs if doesn't exist
-ifelse(!dir.exists(paste0(dir, 'Bird_Phenology/Figures/cubic_halfmax/arrival_', RUN_DATE)), 
-       dir.create(paste0(dir, 'Bird_Phenology/Figures/cubic_halfmax/arrival_', RUN_DATE)), 
+ifelse(!dir.exists(paste0(dir, 'Bird_Phenology/Figures/halfmax/arrival_', RUN_DATE)), 
+       dir.create(paste0(dir, 'Bird_Phenology/Figures/halfmax/arrival_', RUN_DATE)), 
        FALSE)
 
-setwd(paste0(dir, 'Bird_Phenology/Figures/cubic_halfmax/arrival_', RUN_DATE))
+setwd(paste0(dir, 'Bird_Phenology/Figures/halfmax/arrival_', RUN_DATE))
 
 
 #loop through each species, year, cell and extract half-max parameter
