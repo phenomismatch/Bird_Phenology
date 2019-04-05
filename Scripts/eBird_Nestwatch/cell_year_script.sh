45f17541824f1f3d02d4a25ff15888a30b9a0d92
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-04-05 11:30:34 -0400

    update script

--- Scripts/eBird_Nestwatch/cell_year_script.sh
+++ Scripts/eBird_Nestwatch/cell_year_script.sh
@@ -4,5 +4,5 @@ for a in *.out
 do
     cat $a | tail -n 400 | grep species | tail -n 1
     JN=`ls $a | head -c20`
-    qstat -u cyoungflesh | grep $JN
+    qstat -u cyoungflesh | grep $JN | tail -c8
 done
