a621e654682e9874438b873d0dc53cf91f7e1fa6
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-05-07 15:52:46 -0400

    fix batch scripts

--- Scripts/eBird_Nestwatch/2-halfmax-arr/species/2-Empidonax_alnorum.sh
+++ Scripts/eBird_Nestwatch/2-halfmax-arr/species/2-Empidonax_alnorum.sh
@@ -9,8 +9,8 @@
 #SBATCH --mem=50G #memory requested
 #SBATCH --mail-type=END #when to send email (on job completion)
 #SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
-#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_species_2019-05-03/hm-arr-Empidonax_alnorum.out #STDOUT
-#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_species_2019-05-03/hm-arr-Empidonax_alnorum.err #STDERR
+#SBATCH -o /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_species_2019-03-29/hm-arr-Empidonax_alnorum.out #STDOUT
+#SBATCH -e /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_species_2019-03-29/hm-arr-Empidonax_alnorum.err #STDERR
 
 #echos name of node
 echo `hostname`
