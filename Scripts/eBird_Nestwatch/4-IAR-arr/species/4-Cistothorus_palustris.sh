133aabe5b31ecc3a8b355a526563659540050dba
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-05-07 19:42:09 -0400

    new batch scripts

--- Scripts/eBird_Nestwatch/4-IAR-arr/species/4-Cistothorus_palustris.sh
+++ Scripts/eBird_Nestwatch/4-IAR-arr/species/4-Cistothorus_palustris.sh
@@ -17,7 +17,7 @@ echo `hostname`
 
 #load singularity and run R script using singularity
 module load singularity/3.0.2
-singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR/4-IAR-arr.R Cistothorus_palustris
+singularity exec -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /UCHC/LABS/Tingley/phenomismatch/Bird_Phenology/Scripts/eBird_Nestwatch/4-IAR-arr/4-IAR-arr.R Cistothorus_palustris
 
 #displays amount of memory used
 sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
