768336c076a2bcefc7081c26c79e84c08465e44d
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-06-12 19:12:18 -0400

    clean up plots

--- Scripts/eBird_Nestwatch/AOS/pheno_trend_2.R
+++ Scripts/eBird_Nestwatch/AOS/pheno_trend_2.R
@@ -1,4 +1,5 @@
 #calc var for each cell (interannual variability)
+#more variation at bottom edge of range, it seems
 
 
 # top-level dir --------------------------------------------------------------
@@ -17,6 +18,11 @@ library(rstan)
 library(MCMCvis)
 library(dggridR)
 
+
+
+# args --------------------------------------------------------------------
+
+species <- 'Vireo_olivaceus'
 
 
 # read in data produced by 5 ----------------------------------------------------
@@ -54,15 +60,20 @@ ext_ind <- paste0('y_true\\[', d_ind[,1], ',', d_ind[,2], '\\]')
 
 setwd('~/Desktop/Bird_Phenology_Offline/Data/Processed/IAR_output_2019-05-26/')
 
-fit <- readRDS('Vireo_olivaceus-2019-05-26-iar-stan_output.rds')
-fit_in <- readRDS('Vireo_olivaceus-2019-05-26-iar-stan_input.rds')
+fit <- readRDS(paste0(species, '-2019-05-26-iar-stan_output.rds'))
 
 yt_ch_p <- MCMCvis::MCMCpstr(fit, params = 'y_true', type = 'chains')[[1]]
 
-cell_num <- sort(unique(d_ind2$row))
-
 #filter cell lats
+cell_num <- sort(unique(d_ind2$row))
 cl <- cells[cell_num,]
+
+
+
+# read in GAM fit data ----------------------------------------------------
+
+setwd(paste0(dir, 'Bird_Phenology/Data/Processed/halfmax_species_2019-05-03'))
+GAM_halfmax <- readRDS(paste0('halfmax_df_arrival_', species, '.rds'))
 
 
 
@@ -70,19 +81,50 @@ cl <- cells[cell_num,]
 
 #for each iter, sd among years for each cell
 sd_iter <- c()
+dsd_out <- c()
+GAM_sd <- c()
+sd_iter_yr <- c()
 for (i in 1:length(cell_num))
 {
-  #i <- 2
+  #i <- 11
   #get value for each iter for every year at specified cell
   tt <- yt_ch_p[cell_num[i], , ]
+  #only years which there were data
+  yr_idx <- dplyr::filter(d_ind, row == cell_num[i])[,2]
+  tt2 <- yt_ch_p[cell_num[i], yr_idx, ]
+  
+  #IAR input sd
+  # dsd <- sd(dplyr::filter(data, cell == cl$cell[i])$mean_pre_IAR, na.rm = TRUE)
+  # dsd_out <- c(dsd_out, dsd)
+  
+  #GAM sd
+  tt_GAM <- dplyr::filter(GAM_halfmax, cell == cl$cell[i])
+  halfmax <- as.matrix(tt_GAM[,grep('iter', colnames(tt_GAM))])
+  hm_mns <- apply(halfmax, 1, mean)
+  t_GAM_sd <- sd(hm_mns, na.rm = TRUE)
+  GAM_sd <- c(GAM_sd, t_GAM_sd)
+  
   #for each iter calculate sd among years for specified cell
-  t_sd <- apply(tt, 2, sd)
+  if (NCOL(tt) > 1)
+  {
+    t_sd <- apply(tt, 2, sd)
+    sd_iter <- cbind(sd_iter, t_sd)
+  } else {
+    sd_iter <- cbind(sd_iter, rep(NA, 24000))
+  }
+  if (NCOL(tt2) > 2)
+  {
+    t2_sd <- apply(tt2, 2, sd)
+    sd_iter_yr <- cbind(sd_iter_yr, t2_sd)
+  } else {
+    sd_iter_yr <- cbind(sd_iter_yr, rep(NA, 24000))
+  }
 
   #hist(t_sd, xlim = c(0, 7), breaks = 20)
-  sd_iter <- cbind(sd_iter, t_sd)
 }
 
 colnames(sd_iter) <- cl$cell
+colnames(sd_iter_yr) <- cl$cell
 
 #cell numbers and lat sorted by lat
 cl_srt <- cl[order(cl[,2]),]
@@ -92,29 +134,49 @@ cl_srt <- cl[order(cl[,2]),]
 # slope estimate for each iter --------------------------------------------
 
 sl_out <- c()
+sl_out2 <- c()
 for (j in 1:NROW(sd_iter))
 {
   #j <- 1
   sl <- summary(lm(sd_iter[j,] ~ cl$cell_lat))$coef[2,1]
   sl_out <- c(sl_out, sl)
+  
+  sl2 <- summary(lm(sd_iter_yr[j,] ~ cl$cell_lat))$coef[2,1]
+  sl_out2 <- c(sl_out2, sl2)
 }
 
-hist(sl_out)
+
+# #histogram fig
+# pdf(paste0(species, '_sd_lat_hist.pdf'))
+# hist(sl_out, xlab = 'Slope for sd ~ lat', main = paste0(species))
+# abline(v = 0, lty = 2, lwd = 4)
+# dev.off()
+# 
+# hist(sl_out2, xlab = 'Slope for sd ~ lat', main = paste0(species))
+# abline(v = 0, lty = 2, lwd = 4)
+
 
 
 # plot interannual variabilty ---------------------------------------------
 
-plot(rep(cl$cell_lat[1], 24000), sd_iter[,1], pch = '.',
+#black points are values at each iteration (only years with data)
+#red points are mean
+pdf(paste0(species, '_sd_lat_plot.pdf'))
+plot(rep(cl$cell_lat[1], 24000), sd_iter_yr[,1], pch = '.',
      col = rgb(0,0,0,0.2), xlim = range(cl$cell_lat), ylim = c(0,8),
-     xlab = 'Latitude', ylab = 'Inerannual variability (sd)')
-for (i in 2:NCOL(sd_iter))
+     xlab = 'Latitude', ylab = 'Inerannual variability (sd)',
+     main = species)
+points(cl$cell_lat[1], mean(sd_iter_yr[,1]), cex = 0.5, pch = 19, col = rgb(1,0,0,0.3))
+for (i in 2:NCOL(sd_iter_yr))
 {
   #i <- 2
-  points(rep(cl$cell_lat[i], 24000), sd_iter[,i], pch = '.', 
+  points(rep(cl$cell_lat[i], 24000), sd_iter_yr[,i], pch = '.', 
          col = rgb(0,0,0,0.2))
+  points(cl$cell_lat[i], mean(sd_iter_yr[,i]), cex = 0.5, pch = 19, col = rgb(1,0,0,0.3))
 }
+dev.off()
 
 #same with MCMCplot
-MCMCplot(sd_iter, params = cl_srt$cell, 
+MCMCplot(sd_iter_yr, params = cl_srt$cell,
          horiz = FALSE, labels = cl_srt$cell_lat, ref = NULL)
 
