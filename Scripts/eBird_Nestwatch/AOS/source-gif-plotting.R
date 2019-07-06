5d807ca68558638811a61b59e74b3ef2645165ad
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-07-06 13:24:57 -0400

    add imagemagick code

--- Scripts/eBird_Nestwatch/AOS/source-gif-plotting.R
+++ Scripts/eBird_Nestwatch/AOS/source-gif-plotting.R
@@ -1,4 +1,7 @@
 
+#terminal code for imagemagick:
+# convert -density 300 -delay 100 -resize 1000x1000 *.pdf ~/Desktop/IAR.gif
+
 IAR_species <- as.character(read.table('~/Google_Drive/R/Bird_Phenology/Data/IAR_species_list.txt')[,1])
 
 setwd('~/Google_Drive/R/Bird_Phenology/Scripts/eBird_Nestwatch/AOS/')
