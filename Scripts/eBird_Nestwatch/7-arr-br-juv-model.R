49e05635fc6a0200c34d5143147d1684fc1bdb0e
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-08-22 13:33:51 -0400

    X ~ arr not arr ~ X

--- Scripts/eBird_Nestwatch/7-arr-br-juv-model.R
+++ Scripts/eBird_Nestwatch/7-arr-br-juv-model.R
@@ -63,10 +63,10 @@ sp_idx <- as.numeric(factor(mrg_f2$species))
 
 # Stan model --------------------------------------------------------------
 
-DATA <- list(y = mrg_f2$mean_post_IAR,
-             sd_y = mrg_f2$sd_post_IAR,
-             juv = mrg_f2$juv_mean,
-             sd_juv = mrg_f2$juv_sd,
+DATA <- list(y = mrg_f2$juv_mean,
+             sd_y = mrg_f2$juv_sd,
+             arr = mrg_f2$mean_post_IAR,
+             sd_arr = mrg_f2$mean_post_IAR,
              N = NROW(mrg_f2),
              sp = sp_idx,
              Nsp = length(unique(sp_idx)))
@@ -76,8 +76,8 @@ data {
 int<lower=0> N;                      // number of data points
 vector<lower=0>[N] y;                  // response
 vector<lower=0>[N] sd_y;               // uncertainty in response
-vector<lower=0>[N] juv;
-vector<lower=0>[N] sd_juv;
+vector<lower=0>[N] arr;
+vector<lower=0>[N] sd_arr;
 int<lower=0> sp[N];              
 int<lower=0> Nsp;
 }
@@ -85,7 +85,7 @@ int<lower=0> Nsp;
 parameters {
 real<lower = 0> sigma_raw;
 vector[N] mu_y_raw;
-vector[N] mu_juv_raw;
+vector[N] mu_arr_raw;
 real mu_alpha_raw;
 real mu_beta_raw;
 vector<lower = 0>[2] sigma_sp_raw;
@@ -96,7 +96,7 @@ matrix[2, Nsp] z;                          // z-scores
 transformed parameters {
 real<lower = 0> sigma;
 vector[N] mu_y;
-vector[N] mu_juv;
+vector[N] mu_arr;
 vector[N] mu;
 matrix[Nsp, 2] ab;                              // matrix for alpha, beta, gamma, and theta
 matrix[2, 2] Rho;                                 // covariance matrix
@@ -111,8 +111,8 @@ real mu_beta;
 mu_alpha = mu_alpha_raw * 200;
 mu_beta = mu_beta_raw * 2;
 sigma = sigma_raw * 10;
-mu_juv = mu_juv_raw * 40 + 200;
-sigma_sp[1] = sigma_sp_raw[1] * 20;
+mu_arr = mu_arr_raw * 40 + 180;
+sigma_sp[1] = sigma_sp_raw[1] * 40;
 sigma_sp[2] = sigma_sp_raw[2] * 1;
 
 // cholesky factor of covariance matrix multiplied by z score
@@ -124,7 +124,7 @@ Rho = L_Rho * L_Rho';
 for (i in 1:N)
 {
   mu[i] = (mu_alpha + ab[sp[i], 1]) +
-  (mu_beta + ab[sp[i], 2]) * mu_juv[i];
+  (mu_beta + ab[sp[i], 2]) * mu_arr[i];
 }
 
 alpha_c = mu_alpha + alpha;
@@ -141,13 +141,13 @@ sigma_sp_raw ~ std_normal();
 mu_alpha_raw ~ std_normal();
 mu_beta_raw ~ std_normal();
 mu_y_raw ~ std_normal();
-mu_juv_raw ~ std_normal();
+mu_arr_raw ~ std_normal();
 
 to_vector(z) ~ std_normal();
 L_Rho ~ lkj_corr_cholesky(2);
 
-// observation model for juvs
-juv ~ normal(mu_juv, sd_juv);
+// observation model for arr
+arr ~ normal(mu_arr, sd_arr);
 
 // observation model for arrival
 y ~ normal(mu_y, sd_y);
@@ -185,7 +185,7 @@ fit <- rstan::stan(model_code = stanmodel1,
                             'sigma_sp',
                             'Rho',
                             'sigma',
-                            'mu_juv',
+                            'mu_arr',
                             'mu_y',
                             'y_rep'), 
                    control = list(adapt_delta = DELTA,
@@ -195,8 +195,9 @@ run_time <- (proc.time()[3] - tt[3]) / 60
 
 
 setwd(paste0(dir, 'Bird_Phenology/Data/Processed/arr_br_', juv_date))
-saveRDS(fit, file = paste0('arr-br-juv-stan-output', juv_date, '.rds'))
-
+saveRDS(fit, file = paste0('arr-br-juv-stan-output-', juv_date, '.rds'))
+saveRDS(mrg_f2, file = paste0('mrg-data-juv-', juv_date, '.rds'))
+#fit <- readRDS(paste0('arr-br-juv-stan-output-', juv_date, '.rds'))
 
 num_diverge <- rstan::get_num_divergent(fit)
 num_tree <- rstan::get_num_max_treedepth(fit)
@@ -306,7 +307,7 @@ sink()
 # mu_alpha = mu_alpha_raw * 200;
 # mu_beta = mu_beta_raw * 2;
 # sigma = sigma_raw * 10;
-# mu_juv = mu_juv_raw * 40 + 200;
+# mu_arr = mu_arr_raw * 40 + 180;
 # sigma_sp[1] = sigma_sp_raw[1] * 20;
 # sigma_sp[2] = sigma_sp_raw[2] * 1;
 # 
@@ -328,8 +329,8 @@ MCMCvis::MCMCtrace(fit,
                    pdf = TRUE,
                    filename = paste0('arr-br-juv-', juv_date, '-trace_mu_beta.pdf'))
 
-#sigma_sp[1] ~ HN(0, 20)
-PR_p <- rnorm(10000, 0, 20)
+#sigma_sp[1] ~ HN(0, 40)
+PR_p <- rnorm(10000, 0, 40)
 PR <- PR_p[which(PR_p > 0)]
 MCMCvis::MCMCtrace(fit,
                    params = 'sigma_sp\\[1',
@@ -358,10 +359,10 @@ MCMCvis::MCMCtrace(fit,
                    pdf = TRUE,
                    filename = paste0('arr-br-juv-', juv_date, '-trace_sigma.pdf'))
  
-#mu_juv ~ N(200, 40)
-PR <- rnorm(10000, 200, 40)
+#mu_arr ~ N(180, 40)
+PR <- rnorm(10000, 180, 40)
 MCMCvis::MCMCtrace(fit,
-                   params = 'mu_juv',
+                   params = 'mu_arr',
                    priors = PR,
                    pdf = TRUE,
                    filename = paste0('arr-br-juv-', juv_date, '-trace_mu_juv.pdf'))
@@ -470,14 +471,14 @@ data_vis_fun <- function(SPECIES = 'all')
     # geom_point(data = DATA_PLOT2, aes(mean_x, mean_y, color = sp_id),
     #            inherit.aes = FALSE, size = 3, alpha = 0.3) +
     #observed data
-    geom_errorbar(data = DATA_PLOT2,
-                  aes(x = x_obs, ymin = y_obs_l, ymax = y_obs_u), width = 0.3,
-                  color = 'red', alpha = 0.2) +
-    geom_errorbarh(data = DATA_PLOT2,
-                   aes(y = y_obs, xmin = x_obs_l, xmax = x_obs_u), height = 0.005,
-                   color = 'red', alpha = 0.2) +
-    geom_point(data = DATA_PLOT2, aes(x_obs, y_obs, color = 'red'),
-               inherit.aes = FALSE, size = 3, alpha = 0.3) +
+    # geom_errorbar(data = DATA_PLOT2,
+    #               aes(x = x_obs, ymin = y_obs_l, ymax = y_obs_u), width = 0.3,
+    #               color = 'red', alpha = 0.2) +
+    # geom_errorbarh(data = DATA_PLOT2,
+    #                aes(y = y_obs, xmin = x_obs_l, xmax = x_obs_u), height = 0.005,
+    #                color = 'red', alpha = 0.2) +
+    # geom_point(data = DATA_PLOT2, aes(x_obs, y_obs), color = 'red',
+    #            inherit.aes = FALSE, size = 3, alpha = 0.3) +
     theme_bw() +
     #scale_x_discrete(limits = c(seq(18,30, by = 2))) +
     ylab('True ARR halfmax') +
