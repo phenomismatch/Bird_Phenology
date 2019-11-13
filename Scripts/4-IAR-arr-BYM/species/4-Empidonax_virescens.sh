4deb0092cd652e991273c7fdd80dfa0503376fe2
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-11-13 13:43:21 -0500

    create HPC batch for tests

---
+++ Scripts/4-IAR-arr-BYM/species/4-Empidonax_virescens.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=bym-Empidonax_virescens
+#SBATCH -N 1 #number of tasks
+#SBATCH -n 1 #number of nodes
+#SBATCH -c 6 #cpus
+#SBATCH --qos=general #queue (same as partition)
+#SBATCH --partition=general #partition - can also specify 'himem'
+#SBATCH --mem=15G #memory requested
+#SBATCH --mail-type=END #when to send email (on job completion)
+#SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bym_output_2019-11-13/Empidonax_virescens-bym.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bym_output_2019-11-13/Empidonax_virescens-bym.err #STDERR
+
+#echos name of node
+echo `hostname`
+
+#load R module and run script
+#module load R/3.5.2
+#Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-IAR-arr/4-IAR-arr.R Empidonax_virescens
+
+#until singularity is sorted
+module load singularity/3.0.2
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-IAR-arr-bym/4-IAR-arr-bym.R Empidonax_virescens
+
+#displays amount of memory used
+sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
---
+++ Scripts/4-IAR-arr-sig/species/4-Vireo_flavifrons.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=sig-Vireo_flavifrons
+#SBATCH -N 1 #number of tasks
+#SBATCH -n 1 #number of nodes
+#SBATCH -c 6 #cpus
+#SBATCH --qos=general #queue (same as partition)
+#SBATCH --partition=general #partition - can also specify 'himem'
+#SBATCH --mem=15G #memory requested
+#SBATCH --mail-type=END #when to send email (on job completion)
+#SBATCH --mail-user=casey.youngflesh@uconn.edu #email address for notification
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/sig_output_2019-11-13/Vireo_flavifrons-sig.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/sig_output_2019-11-13/Vireo_flavifrons-sig.err #STDERR
+
+#echos name of node
+echo `hostname`
+
+#load R module and run script
+#module load R/3.5.2
+#Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-IAR-arr/4-IAR-arr.R Vireo_flavifrons
+
+#until singularity is sorted
+module load singularity/3.0.2
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-IAR-arr-sig/4-IAR-arr-sig.R Vireo_flavifrons
+
+#displays amount of memory used
+sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
