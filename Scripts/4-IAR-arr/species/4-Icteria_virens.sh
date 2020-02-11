cf966c0107972908b29a2610f78e47515ceda004
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-02-10 16:08:00 -0800

    batch scripts

---
+++ Scripts/4-IAR-arr/species/4-Icteria_virens.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=2020-02-10-Icteria_virens
+#SBATCH -N 1 #number of tasks
+#SBATCH -n 1 #number of nodes
+#SBATCH -c 6 #cpus
+#SBATCH --qos=general #queue (same as partition)
+#SBATCH --partition=general #partition - can also specify 'himem'
+#SBATCH --mem=15G #memory requested
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2020-02-10/Icteria_virens-iar.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/IAR_output_2020-02-10/Icteria_virens-iar.err #STDERR
+
+#echos name of node
+echo `hostname`
+
+#load R module and run script
+#module load R/3.5.2
+#Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-IAR-arr/4-IAR-arr.R Icteria_virens
+
+#until singularity is sorted
+module load singularity/3.0.2
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-IAR-arr/4-IAR-arr.R Icteria_virens
+
+#displays amount of memory used
+sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
