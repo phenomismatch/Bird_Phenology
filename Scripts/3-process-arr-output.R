bd84f8b2ce71bb705ee61ee3f47f12a82d5729dd
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-02-20 18:21:37 -0800

    update processing based on 2 changes

--- Scripts/3-process-arr-output.R
+++ Scripts/3-process-arr-output.R
@@ -19,7 +19,7 @@ dir <- '~/Google_Drive/R/'
 
 # db/hm query dir ------------------------------------------------------------
 
-hm_dir <- 'halfmax_species_2019-05-03'
+hm_dir <- 'halfmax_arrival_2020-02-XX'
 hm_date <- substr(hm_dir, start = 17, stop = 26)
 
 
@@ -46,16 +46,9 @@ setwd(paste0(dir, 'Bird_Phenology/Data/'))
 
 species_list_i <- read.table('eBird_species_list.txt', stringsAsFactors = FALSE)
 species_list <- species_list_i[,1]
-
-#temporary filter out these species
-# rm_sp <- which(species_list %in% c('Empidonax_virescens',
-#                                    'Empidonax_alnorum', 'Setophaga_americana'))
-# species_list <- species_list[-rm_sp]
 nsp <- length(species_list)
-
 years <- 2002:2018
 nyr <- length(years)
-
 
 
 # Proces halfmax results -----------------------------------------------------------------
@@ -68,7 +61,7 @@ for (i in 1:nsp)
   
   #import halfmax estimates and diagnostics from logit cubic model
   setwd(paste0(dir, 'Bird_Phenology/Data/Processed/', hm_dir))
-  temp_halfmax <- readRDS(paste0('halfmax_df_arrival_', species_list[i], '.rds'))
+  temp_halfmax <- readRDS(paste0('halfmax_arrival_', species_list[i], '.rds'))
 
   if (!is.na(temp_halfmax$year[1]))
   {
@@ -83,7 +76,7 @@ for (i in 1:nsp)
   
   #import halfmax estimates and diagnostics from logit cubic model
   setwd(paste0(dir, 'Bird_Phenology/Data/Processed/', hm_dir))
-  temp_halfmax <- readRDS(paste0('halfmax_df_arrival_', species_list[i], '.rds'))
+  temp_halfmax <- readRDS(paste0('halfmax_arrival_', species_list[i], '.rds'))
   
   if (i == 1)
   {
@@ -94,21 +87,24 @@ for (i in 1:nsp)
                                     cell = na_reps,
                                     HM_mean = na_reps,
                                     HM_sd = na_reps,
+                                    max_Rhat = na_reps,
+                                    min_neff = na_reps,
+                                    mlmax = na_reps,
+                                    slmax = na_reps,
+                                    num_diverge = na_reps,
+                                    num_tree = na_reps,
+                                    num_BFMI = na_reps,
+                                    delta = na_reps,
+                                    tree_depth = na_reps,
+                                    t_iter = na_reps,
                                     n1 = na_reps,
                                     n1W = na_reps,
                                     n0 = na_reps,
                                     n0i = na_reps,
+                                    njd = na_reps,
                                     njd1 = na_reps,
                                     njd0 = na_reps,
-                                    njd0i = na_reps,
-                                    max_Rhat = na_reps,
-                                    min_neff = na_reps,
-                                    nphen_bad = na_reps,
-                                    delta = na_reps,
-                                    tree_depth = na_reps,
-                                    num_diverge = na_reps,
-                                    num_tree = na_reps,
-                                    num_BFMI = na_reps)
+                                    njd0i = na_reps)
   }
   
   #loop through years
@@ -138,37 +134,42 @@ for (i in 1:nsp)
         print(paste0('species: ', species_list[i], ', ',
                      'year: ', years[j], ', ', 
                      'cell: ', cells[k]))
-
+        
+        diagnostics_frame$min_neff[counter] <- tt_halfmax2$min_neff
+        diagnostics_frame$max_Rhat[counter] <- tt_halfmax2$max_Rhat
+        #did mean have local max
+        diagnostics_frame$mlmax[counter] <- tt_halfmax2$mlmax
+        #number of iter with local max
+        diagnostics_frame$slmax[counter] <- tt_halfmax2$slmax
+        diagnostics_frame$num_diverge[counter] <- tt_halfmax2$num_diverge
+        diagnostics_frame$num_tree[counter] <- tt_halfmax2$num_tree
+        diagnostics_frame$num_BFMI[counter] <- tt_halfmax2$num_BFMI
+        diagnostics_frame$delta[counter] <- tt_halfmax2$delta
+        diagnostics_frame$tree_depth[counter] <- tt_halfmax2$tree_depth
+        #total iterations
+        diagnostics_frame$t_iter[counter] <- tt_halfmax2$t_iter
         #number of surveys where species was detected
         diagnostics_frame$n1[counter] <- tt_halfmax2$n1
-        #number of surveys where species was not detected
-        diagnostics_frame$n0[counter] <- tt_halfmax2$n0
         #number of detections that came before jday 60
         diagnostics_frame$n1W[counter] <- tt_halfmax2$n1W
+        #number of surveys where species was not detected
+        diagnostics_frame$n0[counter] <- tt_halfmax2$n0
         #number of non-detections before first detection
         diagnostics_frame$n0i[counter] <- tt_halfmax2$n0i
+        #number of julian days with obs
+        diagnostics_frame$njd[counter] <- tt_halfmax2$njd
         #number of unique days with detections
         diagnostics_frame$njd1[counter] <- tt_halfmax2$njd1
         #number of unique days with non-detection
         diagnostics_frame$njd0[counter] <- tt_halfmax2$njd0
         #number of unique days of non-detections before first detection
         diagnostics_frame$njd0i[counter] <- tt_halfmax2$njd0i
-          
-        diagnostics_frame$min_neff[counter] <- tt_halfmax2$min_neff
-        diagnostics_frame$max_Rhat[counter] <- tt_halfmax2$max_Rhat
         
-        diagnostics_frame$delta[counter] <- tt_halfmax2$delta
-        diagnostics_frame$tree_depth[counter] <- tt_halfmax2$tree_depth
-        diagnostics_frame$num_diverge[counter] <- tt_halfmax2$num_diverge
-        diagnostics_frame$num_tree[counter] <- tt_halfmax2$num_tree
-        diagnostics_frame$num_BFMI[counter] <- tt_halfmax2$num_BFMI
-        
-        iter_ind <- grep('iter', colnames(tt_halfmax2))
-        halfmax_posterior <- as.numeric(tt_halfmax2[,iter_ind])
-        
-        #determine how many estimates are 1 and not 1 (estimates of 1 are bogus)
-        diagnostics_frame$nphen_bad[counter] <- sum(halfmax_posterior == 1)
-        #halfmax_posterior2 <- halfmax_posterior[which(halfmax_posterior != 1)]
+        #posterior for halfmax
+        cndf <- colnames(tt_halfmax2)
+        iter_ind <- grep('iter', cndf)
+        to.rm <- which(cndf == 't_iter')
+        halfmax_posterior <- as.numeric(tt_halfmax2[,iter_ind[-which(iter_ind == to.rm)]])
         
         #calculate posterior mean and sd
         if (sum(!is.na(halfmax_posterior)) > 0)
@@ -176,7 +177,6 @@ for (i in 1:nsp)
           diagnostics_frame$HM_mean[counter] <- mean(halfmax_posterior)
           diagnostics_frame$HM_sd[counter] <- sd(halfmax_posterior)
         }
-        
         counter <- counter + 1
       } # if loop for at least one cell - species without sufficient ranges have 0 cells
     } # k -cell
@@ -192,15 +192,15 @@ diagnostics_frame2 <- diagnostics_frame
 
 
 ### add NA for both HM_mean and HM_sd if any of the following conditions are met
-#these params used for most recent IAR input run (as of 2019-07-23)
+#these params used for most recent IAR input run (as of 2020-02-XX)
 
 to.NA <- which(diagnostics_frame2$num_diverge > 0 | 
-                 diagnostics_frame2$max_Rhat >= 1.1 |
-                 diagnostics_frame2$min_neff < 200 |
+                 diagnostics_frame2$max_Rhat >= 1.05 |
+                 diagnostics_frame2$min_neff < 350 |
                  diagnostics_frame2$num_BFMI > 0 |
-                 diagnostics_frame2$HM_sd > 10)
+                 diagnostics_frame2$HM_sd > 15)
 
-# #1.4% of cells are bad
+# #XX% of cells are bad
 # length(to.NA)/sum(!is.na(diagnostics_frame2$HM_mean))
 # diagnostics_frame2[to.NA,c('species', 'cell', 'year',
 #                            'HM_mean', 'HM_sd', 'min_neff', 'num_diverge')]
