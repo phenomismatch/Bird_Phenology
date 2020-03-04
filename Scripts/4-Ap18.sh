7f87fca2ef7728d0274588533c099e91ddcd8e2e
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-03-04 11:13:40 -0800

    batch for test

--- Scripts/4-Ap18.sh
+++ Scripts/4-Ap18.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#SBATCH --job-name=Ap17
+#SBATCH --job-name=Ap18
 #SBATCH -N 1 #number of tasks
 #SBATCH -n 1 #number of nodes
 #SBATCH -c 6 #cpus
 #SBATCH --qos=general #queue (same as partition)
 #SBATCH --partition=general #partition - can also specify 'himem'
 #SBATCH --mem=15G #memory requested
-#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/4-test/Ap17.out #STDOUT
-#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/4-test/Ap17.err #STDERR
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/4-test/Ap18.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/4-test/Ap18.err #STDERR
 
 #echos name of node
 echo `hostname`
 
 #until singularity is sorted
 module load singularity/3.0.2
-singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-test17.R
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/4-test18.R
 
 #displays amount of memory used
 sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
