d6d51a2164a54fa73070e4045f15802023777303
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-03-23 17:13:57 -0700

    all cells, even those with no ebird data

--- Scripts/3-process-arr-output.R
+++ Scripts/3-process-arr-output.R
@@ -20,6 +20,7 @@ dir <- '~/Google_Drive/R/'
 # db/hm query dir ------------------------------------------------------------
 
 hm_dir <- 'halfmax_arrival_2020-02-26'
+hm_dir <- 'halfmax_species_2019-05-03'
 hm_date <- substr(hm_dir, start = 17, stop = 26)
 
 
@@ -63,15 +64,10 @@ cmap <- maptools::map2SpatialPolygons(combmap, IDs = IDs,
 cmap2 <- sp::spTransform(cmap, sp::CRS("+proj=laea +lat_0=0 +lon_0=-70 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
 
 #calculate overlap between each hex cell and map
-ovr_df <- data.frame(cell = as.numeric(as.character(hex_shp@data$cell)), per_ovr = NA)
+hex_shp_cells <- as.numeric(as.character(hex_shp@data$cell))
+ovr_df <- data.frame(cell = hex_shp_cells, per_ovr = NA)
 for (i in 1:length(hex_shp))
 {
-  #i <- 151 #676
-  #i <- 167 #734
-  #i <- 168 #735
-  #i <- 157 #703
-  #i <- 149 #674
-  
   #just one hex cell
   hex_spoly <- sp::SpatialPolygons(list(hex_shp@polygons[[i]]))
   proj4string(hex_spoly) <- sp::CRS(proj4string(hex_shp))
@@ -113,33 +109,37 @@ nyr <- length(years)
 # Proces halfmax results -----------------------------------------------------------------
 
 #get number of cell/years
-cell_years <- 0
+tu_cells <- rep(NA, nsp)
 for (i in 1:nsp)
 {
-  #i <- 110
+  #i <- 20
   
   #import halfmax estimates and diagnostics from logit cubic model
   setwd(paste0(dir, 'Bird_Phenology/Data/Processed/', hm_dir))
   temp_halfmax <- readRDS(paste0('halfmax_arrival_', species_list[i], '.rds'))
 
-  if (!is.na(temp_halfmax$year[1]))
-  {
-    cell_years <- cell_years + NROW(temp_halfmax)
-  }
+  tu_cells[i] <- length(unique(temp_halfmax$cell))
 }
+
+'%ni%' <- Negate('%in%')
+
+#reference key for species synonyms for ranges
+setwd(paste0(dir, 'Bird_Phenology/Data/BirdLife_range_maps/metadata/'))
+sp_key <- read.csv('species_filenames_key.csv')
+
 
 counter <- 1
 for (i in 1:nsp)
 {
-  #i <- 1
+  #i <- 20
   
-  #import halfmax estimates and diagnostics from logit cubic model
+  #import halfmax estimates and diagnostics from GAM
   setwd(paste0(dir, 'Bird_Phenology/Data/Processed/', hm_dir))
   temp_halfmax <- readRDS(paste0('halfmax_arrival_', species_list[i], '.rds'))
   
   if (i == 1)
   {
-    na_reps <- rep(NA, cell_years)
+    na_reps <- rep(NA, max(tu_cells) * nyr * nsp)
     
     diagnostics_frame <- data.frame(species = na_reps,
                                     year = na_reps,
@@ -166,16 +166,117 @@ for (i in 1:nsp)
                                     njd0i = na_reps)
   }
   
+  #filter by breeding/migration cells
+  #match species name to shp file name
+  g_ind <- grep(species_list[i], sp_key$file_names_2016)
+  
+  #check for synonyms if there are no matches
+  if (length(g_ind) == 0)
+  {
+    g_ind2 <- grep(species_list[i], sp_key$BL_Checklist_name)
+  } else {
+    g_ind2 <- g_ind
+  }
+  
+  #change dir to shp files
+  setwd(paste0(dir, 'Bird_Phenology/Data/BirdLife_range_maps/shapefiles/'))
+  #get filename and read in
+  fname <- as.character(sp_key[g_ind2,]$filenames[grep('.shp', sp_key[g_ind2, 'filenames'])])
+  #read in shp file
+  sp_rng <- rgdal::readOGR(fname[1], verbose = FALSE)
+  #to avoid self-intersection problem
+  sp_rng2 <- rgeos::gBuffer(sp_rng, byid = TRUE, width = 0)
+  #crop to area of interest
+  sp_rng3 <- raster::crop(sp_rng2, raster::extent(-95, -50, 24, 90))
+  
+  #filter by breeding (2) and migration (4) range - need to convert spdf to sp
+  nrng <- sp_rng3[which(sp_rng3$SEASONAL == 2 | sp_rng3$SEASONAL == 4),]
+  
+  #filter by resident (1) and non-breeding (3) to exclude hex cells that contain 2/4 and 1/3
+  nrng_rm <- sp_rng3[which(sp_rng3$SEASONAL == 1 | sp_rng3$SEASONAL == 3),]
+
+  #remove unneeded objects
+  rm(sp_rng)
+  rm(sp_rng2)
+  rm(sp_rng3)
+
+  #if there is a legitimate range
+  if (NROW(nrng@data) > 0)
+  {
+    #good cells
+    nrng_sp <- sp::SpatialPolygons(nrng@polygons)
+    sp::proj4string(nrng_sp) <- sp::CRS(sp::proj4string(nrng))
+    #find intersections with code from here: https://gis.stackexchange.com/questions/140504/extracting-intersection-areas-in-r
+    poly_int <- rgeos::gIntersects(hex_shp, nrng_sp, byid = TRUE)
+    tpoly <- which(poly_int == TRUE, arr.ind = TRUE)[,2]
+    br_mig_cells <- hex_shp_cells[as.numeric(tpoly[!duplicated(tpoly)])]
+  
+    #bad cells - also exclude cells 812, 813, and 841 (Bahamas)
+    if (length(nrng_rm) > 0)
+    {
+      nrng_rm_sp <- sp::SpatialPolygons(nrng_rm@polygons)
+      sp::proj4string(nrng_rm_sp) <- sp::CRS(sp::proj4string(nrng_rm))
+      poly_int_rm <- rgeos::gIntersects(hex_shp, nrng_rm_sp, byid = TRUE)
+      tpoly_rm <- which(poly_int_rm == TRUE, arr.ind = TRUE)[,2]
+      res_ovr_cells <- hex_shp_cells[as.numeric(tpoly_rm[!duplicated(tpoly_rm)])]
+      
+      #remove cells that appear in resident and overwinter range that also appear in breeding range
+      cell_mrg <- c(br_mig_cells, res_ovr_cells)
+      to_rm <- c(cell_mrg[duplicated(cell_mrg)], 812, 813, 841)
+      
+      rm(nrng_rm)
+      rm(nrng_rm_sp)
+      rm(res_ovr_cells)
+      
+    } else {
+      cell_mrg <- br_mig_cells
+      to_rm <- c(812, 813, 841)
+    }
+    
+    #remove unneeded objects
+    rm(nrng)
+    rm(nrng_sp)
+    
+    c_rm <- which(br_mig_cells %in% to_rm)
+    if (length(c_rm) > 0)
+    {
+      overlap_cells <- br_mig_cells[-c_rm]  
+    } else {
+      overlap_cells <- br_mig_cells
+    }
+    
+    #remove unneeded objects
+    rm(br_mig_cells)
+    rm(cell_mrg)
+    rm(to_rm)
+    
+    #get cell centers
+    cell_centers <- dggridR::dgSEQNUM_to_GEO(hexgrid6, overlap_cells)
+    cc_df <- data.frame(cell = overlap_cells, lon = cell_centers$lon_deg, 
+                        lat = cell_centers$lat_deg)
+    
+    #remove unneeded objects
+    rm(cell_centers)
+    rm(overlap_cells)
+    
+    #cells only within the range that ebird surveys were filtered to
+    n_cc_df <- cc_df[which(cc_df$lon > -95 & cc_df$lon < -50 & cc_df$lat > 24),]
+    cells <- n_cc_df$cell
+    ncell <- length(cells)
+    
+    #remove unneeded objects
+    rm(cc_df)
+    rm(n_cc_df)
+  } else {
+    ncell <- NA
+  } 
+  
   #loop through years
   for (j in 1:nyr)
   {
     #j <- 1
     tt_halfmax1 <- dplyr::filter(temp_halfmax, 
                                  year == years[j])
-    
-    cells <- unique(tt_halfmax1$cell)
-    ncell <- length(cells)
-    
     if (ncell > 0)
     {
       for (k in 1:ncell)
@@ -185,6 +286,7 @@ for (i in 1:nsp)
         diagnostics_frame$species[counter] <- species_list[i]
         diagnostics_frame$year[counter] <- years[j]
         diagnostics_frame$cell[counter] <- cells[k]
+        diagnostics_frame$shp_fname[counter] <- fname
         
         #get model fits
         tt_halfmax2 <- dplyr::filter(tt_halfmax1, 
@@ -194,53 +296,63 @@ for (i in 1:nsp)
                      'year: ', years[j], ', ', 
                      'cell: ', cells[k]))
         
-        diagnostics_frame$min_neff[counter] <- tt_halfmax2$min_neff
-        diagnostics_frame$max_Rhat[counter] <- tt_halfmax2$max_Rhat
-        #did mean have local max
-        diagnostics_frame$mlmax[counter] <- tt_halfmax2$mlmax
-        #percent of iter with local max
-        diagnostics_frame$plmax[counter] <- tt_halfmax2$plmax
-        diagnostics_frame$num_diverge[counter] <- tt_halfmax2$num_diverge
-        diagnostics_frame$num_tree[counter] <- tt_halfmax2$num_tree
-        diagnostics_frame$num_BFMI[counter] <- tt_halfmax2$num_BFMI
-        diagnostics_frame$delta[counter] <- tt_halfmax2$delta
-        diagnostics_frame$tree_depth[counter] <- tt_halfmax2$tree_depth
-        #total iterations
-        diagnostics_frame$t_iter[counter] <- tt_halfmax2$t_iter
-        #number of surveys where species was detected
-        diagnostics_frame$n1[counter] <- tt_halfmax2$n1
-        #number of detections that came before jday 60
-        diagnostics_frame$n1W[counter] <- tt_halfmax2$n1W
-        #number of surveys where species was not detected
-        diagnostics_frame$n0[counter] <- tt_halfmax2$n0
-        #number of non-detections before first detection
-        diagnostics_frame$n0i[counter] <- tt_halfmax2$n0i
-        #number of julian days with obs
-        diagnostics_frame$njd[counter] <- tt_halfmax2$njd
-        #number of unique days with detections
-        diagnostics_frame$njd1[counter] <- tt_halfmax2$njd1
-        #number of unique days with non-detection
-        diagnostics_frame$njd0[counter] <- tt_halfmax2$njd0
-        #number of unique days of non-detections before first detection
-        diagnostics_frame$njd0i[counter] <- tt_halfmax2$njd0i
-        
-        #posterior for halfmax
-        cndf <- colnames(tt_halfmax2)
-        iter_ind <- grep('iter', cndf)
-        to.rm <- which(cndf == 't_iter')
-        halfmax_posterior <- as.numeric(tt_halfmax2[,iter_ind[-which(iter_ind == to.rm)]])
-        
-        #calculate posterior mean and sd
-        if (sum(!is.na(halfmax_posterior)) > 0)
+        if (NROW(tt_halfmax2) > 0)
         {
-          diagnostics_frame$HM_mean[counter] <- mean(halfmax_posterior)
-          diagnostics_frame$HM_sd[counter] <- sd(halfmax_posterior)
+          diagnostics_frame$min_neff[counter] <- tt_halfmax2$min_neff
+          diagnostics_frame$max_Rhat[counter] <- tt_halfmax2$max_Rhat
+          #did mean have local max
+          diagnostics_frame$mlmax[counter] <- tt_halfmax2$mlmax
+          #percent of iter with local max
+          diagnostics_frame$plmax[counter] <- tt_halfmax2$plmax
+          diagnostics_frame$num_diverge[counter] <- tt_halfmax2$num_diverge
+          diagnostics_frame$num_tree[counter] <- tt_halfmax2$num_tree
+          diagnostics_frame$num_BFMI[counter] <- tt_halfmax2$num_BFMI
+          diagnostics_frame$delta[counter] <- tt_halfmax2$delta
+          diagnostics_frame$tree_depth[counter] <- tt_halfmax2$tree_depth
+          #total iterations
+          diagnostics_frame$t_iter[counter] <- tt_halfmax2$t_iter
+          #number of surveys where species was detected
+          diagnostics_frame$n1[counter] <- tt_halfmax2$n1
+          #number of detections that came before jday 60
+          diagnostics_frame$n1W[counter] <- tt_halfmax2$n1W
+          #number of surveys where species was not detected
+          diagnostics_frame$n0[counter] <- tt_halfmax2$n0
+          #number of non-detections before first detection
+          diagnostics_frame$n0i[counter] <- tt_halfmax2$n0i
+          #number of julian days with obs
+          diagnostics_frame$njd[counter] <- tt_halfmax2$njd
+          #number of unique days with detections
+          diagnostics_frame$njd1[counter] <- tt_halfmax2$njd1
+          #number of unique days with non-detection
+          diagnostics_frame$njd0[counter] <- tt_halfmax2$njd0
+          #number of unique days of non-detections before first detection
+          diagnostics_frame$njd0i[counter] <- tt_halfmax2$njd0i
+          
+          #posterior for halfmax
+          cndf <- colnames(tt_halfmax2)
+          iter_ind <- grep('iter', cndf)
+          to.rm <- which(cndf == 't_iter')
+          halfmax_posterior <- as.numeric(tt_halfmax2[,iter_ind[-which(iter_ind == to.rm)]])
+        
+          #calculate posterior mean and sd
+          if (sum(!is.na(halfmax_posterior)) > 0)
+          {
+            diagnostics_frame$HM_mean[counter] <- mean(halfmax_posterior)
+            diagnostics_frame$HM_sd[counter] <- sd(halfmax_posterior)
+          }
         }
         counter <- counter + 1
       } # if loop for at least one cell - species without sufficient ranges have 0 cells
     } # k -cell
   } # j - year
 } # i - species
+
+
+#strip excess NAs
+
+
+#ADD HERE
+
 
 
 #add 'meets criteria' column
@@ -288,15 +400,6 @@ diagnostics_frame3 <- dplyr::left_join(diagnostics_frame2, ovr_df, by = 'cell')
 
 NC <- 3
 
-'%ni%' <- Negate('%in%')
-
-#reference key for species synonyms
-setwd(paste0(dir, 'Bird_Phenology/Data/BirdLife_range_maps/metadata/'))
-sp_key <- read.csv('species_filenames_key.csv')
-
-#change dir to shp files
-setwd(paste0(dir, 'Bird_Phenology/Data/BirdLife_range_maps/shapefiles/'))
-
 df_out <- data.frame()
 #which species/years meet criteria for model
 for (i in 1:length(species_list))
@@ -309,21 +412,6 @@ for (i in 1:length(species_list))
     
     if (NROW(t_sp) > 0)
     {
-      #match species name to shp file name
-      g_ind <- grep(species_list[i], sp_key$file_names_2016)
-    
-      #check for synonyms if there are no matches
-      if (length(g_ind) == 0)
-      {
-        g_ind2 <- grep(species_list[i], sp_key$BL_Checklist_name)
-      } else {
-        g_ind2 <- g_ind
-      }
-    
-      #get filename to put in df
-      fname <- as.character(sp_key[g_ind2,]$filenames[grep('.shp', sp_key[g_ind2, 'filenames'])])
-      t_sp$shp_fname <- fname
-    
       #number of cells with good data in each year from 2016-2018
       nobs_yr <- c()
       for (j in 2016:2018)
