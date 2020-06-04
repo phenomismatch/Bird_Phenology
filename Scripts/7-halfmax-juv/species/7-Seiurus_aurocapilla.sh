4b763f1f795dc60167cf856ec99d8926cdb948b5
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-06-04 14:40:57 -0700

    new HPC batch

---
+++ Scripts/7-halfmax-juv/species/7-Seiurus_aurocapilla.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=hm-juv-Seiurus_aurocapilla
+#SBATCH -N 1 #number of tasks
+#SBATCH -n 1 #number of nodes
+#SBATCH -c 4 #cpus
+#SBATCH --qos=general #queue (same as partition)
+#SBATCH --partition=general #partition - can also specify 'himem'
+#SBATCH --mem=25G #memory requested
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_juv_2020-06-04/hm-juv-Seiurus_aurocapilla.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_juv_2020-06-04/hm-juv-Seiurus_aurocapilla.err #STDERR
+
+#echos name of node
+echo `hostname`
+
+module load gcc/6.4.0
+module load singularity/3.0.2
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/7-halfmax-juv/7-halfmax-juv.R Seiurus_aurocapilla
+
+#displays amount of memory used
+sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
---
+++ Scripts/8-halfmax-br/species/8-Stelgidopteryx_serripennis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=hm-br-Stelgidopteryx_serripennis
+#SBATCH -N 1 #number of tasks
+#SBATCH -n 1 #number of nodes
+#SBATCH -c 4 #cpus
+#SBATCH --qos=general #queue (same as partition)
+#SBATCH --partition=general #partition - can also specify 'himem'
+#SBATCH --mem=25G #memory requested
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_breeding_2020-06-04/hm-br-Stelgidopteryx_serripennis.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/halfmax_breeding_2020-06-04/hm-br-Stelgidopteryx_serripennis.err #STDERR
+
+#echos name of node
+echo `hostname`
+
+module load gcc/6.4.0
+module load singularity/3.0.2
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/8-halfmax-br/8-halfmax-br.R Stelgidopteryx_serripennis
+
+#displays amount of memory used
+sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
