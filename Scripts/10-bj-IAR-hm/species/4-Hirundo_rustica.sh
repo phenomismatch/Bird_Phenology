36e39ae3923e147dd3331519823ac7b040758105
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-08-27 11:11:51 -0700

    HPC prep

---
+++ Scripts/10-bj-IAR-hm/species/4-Hirundo_rustica.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=bj-hm-2020-08-27-Hirundo_rustica
+#SBATCH -N 1 #number of tasks
+#SBATCH -n 1 #number of nodes
+#SBATCH -c 4 #cpus
+#SBATCH --qos=general #queue (same as partition)
+#SBATCH --partition=general #partition - can also specify 'himem'
+#SBATCH --mem=10G #memory requested
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bj_IAR_hm_2020-08-27/Hirundo_rustica-bj-iar-hm.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bj_IAR_hm_2020-08-27/Hirundo_rustica-bj-iar-hm.err #STDERR
+
+#echos name of node
+echo `hostname`
+
+module load gcc/6.4.0
+module load singularity/3.0.2
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/10-bj-IAR-hm/10-bj-IAR-hm.R Hirundo_rustica 5000
+
+#displays amount of memory used
+sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
