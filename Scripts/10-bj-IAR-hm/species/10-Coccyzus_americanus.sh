9773ee9f4f5980b3c6648596f0e581a719295e5a
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-12-28 17:34:02 -0500

    new HPC run

--- Scripts/10-bj-IAR-hm/species/10-Coccyzus_americanus.sh
+++ Scripts/10-bj-IAR-hm/species/10-Coccyzus_americanus.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#SBATCH --job-name=bj-hm-2020-12-07-Coccyzus_americanus
+#SBATCH --job-name=bj-hm-2020-12-28-Coccyzus_americanus
 #SBATCH -N 1 #number of tasks
 #SBATCH -n 1 #number of nodes
 #SBATCH -c 4 #cpus
 #SBATCH --qos=general #queue (same as partition)
 #SBATCH --partition=general #partition - can also specify 'himem'
 #SBATCH --mem=15G #memory requested
-#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bj_IAR_hm_2020-12-07/Coccyzus_americanus-bj-iar-hm.out #STDOUT
-#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bj_IAR_hm_2020-12-07/Coccyzus_americanus-bj-iar-hm.err #STDERR
+#SBATCH -o /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bj_IAR_hm_2020-12-28/Coccyzus_americanus-bj-iar-hm.out #STDOUT
+#SBATCH -e /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bj_IAR_hm_2020-12-28/Coccyzus_americanus-bj-iar-hm.err #STDERR
 
 #echos name of node
 echo `hostname`
 
 module load gcc/6.4.0
 module load singularity/3.0.2
-singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/10-bj-IAR-hm/10-bj-IAR-hm.R Coccyzus_americanus 10000
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/10-bj-IAR-hm/10-bj-IAR-hm.R Coccyzus_americanus 20000
 
 #displays amount of memory used
 sstat --format="AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize" $SLURM_JOBID.batch
