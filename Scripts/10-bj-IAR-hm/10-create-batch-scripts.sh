e14917da16c81afe9dcc9e4e535ac362672aa1e2
Casey Youngflesh <caseyyoungflesh@gmail.com>
2020-12-28 17:33:45 -0500

    update batch creation

--- Scripts/10-bj-IAR-hm/10-create-batch-scripts.sh
+++ Scripts/10-bj-IAR-hm/10-create-batch-scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATE="2020-12-07"
+DATE="2020-12-28"
 
 while read name
 do
@@ -23,7 +23,7 @@ echo \`hostname\`
 
 module load gcc/6.4.0
 module load singularity/3.0.2
-singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/10-bj-IAR-hm/10-bj-IAR-hm.R $temp 10000
+singularity exec -B /labs/Tingley -B /UCHC /isg/shared/apps/R/3.5.2/R.sif Rscript /labs/Tingley/phenomismatch/Bird_Phenology/Scripts/10-bj-IAR-hm/10-bj-IAR-hm.R $temp 20000
 
 #displays amount of memory used
 sstat --format=\"AveCPU,AvePages,AveRSS,MaxRSS,AveVMSize,MaxVMSize\" \$SLURM_JOBID.batch" > "species/10-$temp.sh"
--- Scripts/10-bj-IAR-hm/10-master-submit.sh
+++ Scripts/10-bj-IAR-hm/10-master-submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATE="2020-12-07"
+DATE="2020-12-28"
 mkdir /labs/Tingley/phenomismatch/Bird_Phenology/Data/Processed/bj_IAR_hm_$DATE
 
 while read name
